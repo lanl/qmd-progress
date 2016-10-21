6181e9cd8e19629a43bdc81c9103284c5f743e0d
Christian Negre <cnegre@lanl.gov>
2016-10-21 17:55:56 -0600

    automatic example build

--- example_build.sh
+++ example_build.sh
@@ -4,12 +4,17 @@
 
 rm -r build
 
+MY_PATH=`pwd`
+cd ../
+HOME_PATH=`pwd`
+cd $MY_PATH
+
 CC=gcc \
 FC=gfortran \
 BLAS_VENDOR=MKL \
-PKG_CONFIG_PATH=/home/USER/bml/install/lib64/pkgconfig PROGRESS_OPENMP=yes \
+PKG_CONFIG_PATH=$HOME_PATH/bml/install/lib64/pkgconfig PROGRESS_OPENMP=yes \
 PROGRESS_GRAPHLIB=yes \
-EXTRA_LINK_FLAGS="-L/home/USER/metis-4.1.0/build/Linux-x86_64/libmetis
+EXTRA_LINK_FLAGS="-L"$HOME_PATH"/metis-4.1.0/build/Linux-x86_64/libmetis
 -lmetis" \
 PROGRESS_TESTING=yes CMAKE_BUILD_TYPE=Release FC=gfortran \
 PROGRESS_EXAMPLES=yes \
