d3d762960f6efc623b0305e7387a9074043719fc
Christian Negre <cnegre@lanl.gov>
2017-02-03 13:27:20 -0700

    example_build

--- example_build.sh
+++ example_build.sh
@@ -7,12 +7,12 @@ rm -r install
 
 # Set METIS and BML Library locations
 METIS_LIB="/usr/local/lib"
-BML_LIB="/home/username/bml/lib"
+BML_LIB="$HOME/bml/lib"
 
 MY_PATH=`pwd`
 
 # Configuring PROGRESS with OpenMP and METIS Graph Library
-CC=gcc FC=gfortran BLAS_VENDOR=MKL PKG_CONFIG_PATH=$BML_LIB/pkgconfig PROGRESS_OPENMP=yes INSTALL_DIR="$MY_PATH/install" PROGRESS_GRAPHLIB=yes EXTRA_LINK_FLAGS="-L$METIS_LIB -lmetis" PROGRESS_TESTING=yes CMAKE_BUILD_TYPE=Release PROGRESS_EXAMPLES=yes ./build.sh configure
+CC=mpicc FC=mpif90 BLAS_VENDOR=MKL PKG_CONFIG_PATH=$BML_LIB/pkgconfig PROGRESS_OPENMP=yes INSTALL_DIR="$MY_PATH/install" PROGRESS_GRAPHLIB=yes EXTRA_LINK_FLAGS="-L$METIS_LIB -lmetis" PROGRESS_TESTING=yes CMAKE_BUILD_TYPE=Release PROGRESS_EXAMPLES=yes ./build.sh configure
 
 # Make PROGRESS library and examples
 # cd build
