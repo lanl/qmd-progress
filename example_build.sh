cefdcb90bbe2742f7e8fd922d1e1eddcdc9cc409
Christian Negre <cnegre@lanl.gov>
2017-02-07 12:17:49 -0700

    tr

--- example_build.sh
+++ example_build.sh
@@ -11,8 +11,12 @@ BML_LIB="$HOME/bml/install/lib"
 
 MY_PATH=`pwd`
 
+# Configuring PROGRESS with OpenMP
+CC=gcc FC=gfortran BLAS_VENDOR=GNU PKG_CONFIG_PATH=$BML_LIB/pkgconfig PROGRESS_OPENMP=yes INSTALL_DIR="$MY_PATH/install" PROGRESS_GRAPHLIB=no PROGRESS_TESTING=yes CMAKE_BUILD_TYPE=Release PROGRESS_EXAMPLES=yes ./build.sh configure
+
+
 # Configuring PROGRESS with OpenMP, MPI and METIS Graph Library
-CC=gcc FC=gfortran BLAS_VENDOR=GNU PKG_CONFIG_PATH=$BML_LIB/pkgconfig PROGRESS_OPENMP=yes PROGRESS_MPI=no INSTALL_DIR="$MY_PATH/install" PROGRESS_GRAPHLIB=no EXTRA_LINK_FLAGS="-L$METIS_LIB -lmetis" PROGRESS_TESTING=yes CMAKE_BUILD_TYPE=Release PROGRESS_EXAMPLES=yes ./build.sh configure
+#CC=mpicc FC=mpifort BLAS_VENDOR=GNU PKG_CONFIG_PATH=$BML_LIB/pkgconfig PROGRESS_OPENMP=yes PROGRESS_MPI=yes INSTALL_DIR="$MY_PATH/install" PROGRESS_GRAPHLIB=yes EXTRA_LINK_FLAGS="-L$METIS_LIB -lmetis" PROGRESS_TESTING=yes CMAKE_BUILD_TYPE=Release PROGRESS_EXAMPLES=yes ./build.sh configure
 
 # Make PROGRESS library and examples
 # cd build
