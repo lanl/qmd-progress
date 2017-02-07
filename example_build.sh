11e020827b5f05e230f6492605f31c1f80750a0a
Christian Negre <cnegre@lanl.gov>
2017-02-07 08:30:06 -0700

    example build non MPI

--- example_build.sh
+++ example_build.sh
@@ -6,13 +6,13 @@ rm -r build
 rm -r install 
 
 # Set METIS and BML Library locations
-METIS_LIB="/usr/local/lib"
+METIS_LIB="$HOME/metis-5.1.0/build/Linux-x86_64/libmetis"
 BML_LIB="$HOME/bml/lib"
 
 MY_PATH=`pwd`
 
-# Configuring PROGRESS with OpenMP and METIS Graph Library
-CC=mpicc FC=mpif90 BLAS_VENDOR=MKL PKG_CONFIG_PATH=$BML_LIB/pkgconfig PROGRESS_OPENMP=yes INSTALL_DIR="$MY_PATH/install" PROGRESS_GRAPHLIB=yes EXTRA_LINK_FLAGS="-L$METIS_LIB -lmetis" PROGRESS_TESTING=yes CMAKE_BUILD_TYPE=Release PROGRESS_EXAMPLES=yes ./build.sh configure
+# Configuring PROGRESS with OpenMP, MPI and METIS Graph Library
+CC=gcc FC=gfortran BLAS_VENDOR=GNU PKG_CONFIG_PATH=$BML_LIB/pkgconfig PROGRESS_OPENMP=yes PROGRESS_MPI=no INSTALL_DIR="$MY_PATH/install" PROGRESS_GRAPHLIB=no EXTRA_LINK_FLAGS="-L$METIS_LIB -lmetis" PROGRESS_TESTING=yes CMAKE_BUILD_TYPE=Release PROGRESS_EXAMPLES=yes ./build.sh configure
 
 # Make PROGRESS library and examples
 # cd build
