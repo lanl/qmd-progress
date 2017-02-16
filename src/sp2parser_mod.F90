f3a91ba8fc8b1fa73c84a01849830d2be3a259ba
Christian Negre <cnegre@lanl.gov>
2017-02-16 15:54:57 -0700

    sp2 run example

--- src/sp2parser_mod.F90
+++ src/sp2parser_mod.F90
@@ -1,26 +1,26 @@
-!> SP2 parser. 
+!> SP2 parser.
 !! \ingroup PROGRESS
 !! This module is used to parse all the neccesary input variables for and SP2
 !! electronic structure solver.
-!! Adding a new input keyword to the parser: 
-!! - If the variable is real, we have to increase nkey_re. 
+!! Adding a new input keyword to the parser:
+!! - If the variable is real, we have to increase nkey_re.
 !! - Add the keyword (character type) in the keyvector_re vector.
 !! - Add a default value (real type) in the valvector_re.
 !! - Define a new variable and pass the value through valvector_re(num)
 !! where num is the position of the new keyword in the vector.
-!! 
+!!
 module sp2parser_mod
 
   use openfiles_mod
   use kernelparser_mod
   use bml
-  
+
   implicit none
- 
+
   private
-  
+
   integer, parameter :: dp = kind(1.0d0)
-    
+
   !> General SP2 solver type
   !!
   type, public :: sp2data_type
@@ -28,31 +28,31 @@ module sp2parser_mod
      integer :: verbose
      integer :: minsp2iter
      integer :: maxsp2iter
-     real(dp) :: sp2tol    
+     real(dp) :: sp2tol
      real(dp) :: threshold
      real(dp) :: bndfil
      integer :: mdim
-     integer :: ndim            
+     integer :: ndim
      character :: sdim(3)
-     real(dp) :: pdim(3)                                       
+     real(dp) :: pdim(3)
      character(20) :: bml_type
      character(10) :: sp2conv
-     character(10) :: flavor     
-  end type sp2data_type 
-  
+     character(10) :: flavor
+  end type sp2data_type
+
   public :: parse_sp2
-   
+
 contains
 
   !> The parser for SP2 solver.
-  !!  
+  !!
   subroutine parse_sp2(sp2data,filename)
 
     implicit none
     type(sp2data_type), intent(inout) :: sp2data
     integer, parameter :: nkey_char = 4, nkey_int = 6, nkey_re = 3, nkey_log = 1
-    character(len=*) :: filename    
-    
+    character(len=*) :: filename
+
     !Library of keywords with the respective defaults.
     character(len=50), parameter :: keyvector_char(nkey_char) = [character(len=100) :: &
       'JobName=', 'BMLType=','SP2Conv=','Flavor=' ]
@@ -60,7 +60,7 @@ contains
       'MyJob'   , 'Dense'   ,'REL', 'Alg2' ]
 
     character(len=50), parameter :: keyvector_int(nkey_int) = [character(len=50) :: &
-    'MDim=', 'VarInt=', 'MinSP2Iter=', 'MaxSP2Iter=','Ndim=','Verbose=']                                   
+    'MDim=', 'VarInt=', 'MinSP2Iter=', 'MaxSP2Iter=','Ndim=','Verbose=']
     integer :: valvector_int(nkey_int) = (/ &
        -1   ,     0    ,      10       ,      100 , 1, 0 /)
 
@@ -70,43 +70,43 @@ contains
          0.0      ,   0.00000001    ,0.0 /)
 
     character(len=50), parameter :: keyvector_log(nkey_log) = [character(len=100) :: &
-      'Log1=']
+      'DUMMY=']
     logical :: valvector_log(nkey_log) = (/&
      .false./)
 
     !Start and stop characters
     character(len=50), parameter :: startstop(2) = [character(len=50) :: &
       'SP2{', '}']
-     
+
     call parsing_kernel(keyvector_char,valvector_char&
     ,keyvector_int,valvector_int,keyvector_re,valvector_re,&
     keyvector_log,valvector_log,trim(filename),startstop)
 
-    !Characters 
+    !Characters
     sp2data%JobName = valvector_char(1)
 
-    if(valvector_char(2) == "Dense")then 
+    if(valvector_char(2) == "Dense")then
        sp2data%bml_type = BML_MATRIX_DENSE
-    elseif(valvector_char(2) == "Ellpack")then 
+    elseif(valvector_char(2) == "Ellpack")then
        sp2data%bml_type = BML_MATRIX_ELLPACK
     endif
     sp2data%sp2conv = valvector_char(3)
     sp2data%flavor = valvector_char(4)
 
-    !Reals         
+    !Reals
     sp2data%threshold = valvector_re(1)
     sp2data%sp2tol = valvector_re(2)
     sp2data%BndFil = valvector_re(3)
 
-    !Logicals    
+    !Logicals
 
     !Integers
     sp2data%mdim = valvector_int(1)
     sp2data%minsp2iter = valvector_int(3)
     sp2data%maxsp2iter = valvector_int(4)
-    sp2data%ndim = valvector_int(5)    
-    sp2data%verbose = valvector_int(6)    
-    
-  end subroutine parse_sp2  
-  
+    sp2data%ndim = valvector_int(5)
+    sp2data%verbose = valvector_int(6)
+
+  end subroutine parse_sp2
+
 end module sp2parser_mod
