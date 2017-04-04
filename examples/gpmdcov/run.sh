95c81563f3baf5c52cc8f1c32042da217398cc75
Nicolas Bock <nicolasbock@gmail.com>
2017-04-04 16:19:40 -0600

    Enable metis library in Travis-CI tests
    Also fix run script to also run if MPI isn't available.

--- examples/gpmdcov/run.sh
+++ examples/gpmdcov/run.sh
@@ -5,11 +5,14 @@ export OMP_NUM_THREADS=${OMP_NUM_THREADS:=8}
 : ${MPIRUN:=mpirun}
 NODES=${1:-1}
 
-${MPIRUN} \
-    -np ${NODES} \
-    --map-by node \
-    -x OMP_NUM_THREADS \
-    ../../build/gpmdcov ./input.in \
-    | tee out
-#../../build/gpmdcov ./input.in | tee  out
+if [[ -x ${MPIRUN} ]]; then
+    ${MPIRUN} \
+        -np ${NODES} \
+        --map-by node \
+        -x OMP_NUM_THREADS \
+        ../../build/gpmdcov ./input.in \
+        | tee out
+else
+    ../../build/gpmdcov ./input.in | tee  out
+fi
 #/usr/local/bin/mpiexec -n 4 -bycore -x 1  ../../build/gpmdcov ./input.in | tee out
