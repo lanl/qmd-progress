796bc2f15ed61f839da907ff6dc9faa6e457a9fa
Nicolas Bock <nicolasbock@gmail.com>
2017-04-22 07:23:09 -0600

    Update run.sh script

--- examples/gpmdcov/run.sh
+++ examples/gpmdcov/run.sh
@@ -1,7 +1,7 @@
 set -u
 
 export OMPI_MCA_opal_paffinity_alone=0
-export OMP_NUM_THREADS=${OMP_NUM_THREADS:=8}
+export OMP_NUM_THREADS=${OMP_NUM_THREADS:=1}
 : ${MPIRUN:=mpirun}
 NODES=${1:-1}
 
@@ -11,8 +11,9 @@ if ${MPIRUN} --version > /dev/null 2>&1; then
         --map-by node \
         --host 10.209.225.48 \
         -x OMP_NUM_THREADS \
-        ../../build/gpmdcov ./input.in \
+        -x LD_LIBRARY_PATH \
+        ../../build/gpmdcov input.in \
         | tee out
 else
-    ../../build/gpmdcov ./input.in | tee  out
+    ../../build/gpmdcov input.in | tee  out
 fi
