9fa866182fdbc2b5f7afeb42728481e1eac9cf25
Nicolas Bock <nicolasbock@gmail.com>
2017-04-22 08:27:58 -0600

    Fix OpenMPI start options
    More recent OpenMPI version start MPI processes in a tree which means
    that ssh key login needs to be enabled in between nodes. The added
    option turns this feature off until we figure out a better way to
    handling this.
    
    Maybe we can tune this by adding some option to ssh to skip host key
    verification.

--- examples/gpmdcov/run.sh
+++ examples/gpmdcov/run.sh
@@ -1,6 +1,8 @@
 set -u
+set -x
 
 export OMPI_MCA_opal_paffinity_alone=0
+export OMPI_MCA_plm_rsh_no_tree_spawn=1
 export OMP_NUM_THREADS=${OMP_NUM_THREADS:=1}
 : ${MPIRUN:=mpirun}
 NODES=${1:-1}
@@ -9,7 +11,7 @@ if ${MPIRUN} --version > /dev/null 2>&1; then
     ${MPIRUN} \
         -np ${NODES} \
         --map-by node \
-        --host 10.209.225.48 \
+        --hostfile hostfile \
         -x OMP_NUM_THREADS \
         -x LD_LIBRARY_PATH \
         ../../build/gpmdcov input.in \
