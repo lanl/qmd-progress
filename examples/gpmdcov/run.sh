51adb546dd952791304319eb14d06b960268a75b
Nicolas Bock <nicolasbock@gmail.com>
2017-04-22 09:03:04 -0600

    Fix OpenMP options

--- examples/gpmdcov/run.sh
+++ examples/gpmdcov/run.sh
@@ -1,9 +1,9 @@
 set -u
 set -x
 
-export OMPI_MCA_opal_paffinity_alone=0
-export OMPI_MCA_plm_rsh_no_tree_spawn=1
-export OMP_NUM_THREADS=${OMP_NUM_THREADS:=1}
+#export OMPI_MCA_opal_paffinity_alone=0
+#export OMPI_MCA_plm_rsh_no_tree_spawn=1
+export OMP_NUM_THREADS=${OMP_NUM_THREADS:-1}
 : ${MPIRUN:=mpirun}
 NODES=${1:-1}
 
@@ -12,6 +12,7 @@ if ${MPIRUN} --version > /dev/null 2>&1; then
         -np ${NODES} \
         --map-by node \
         --hostfile hostfile \
+        --mca plm_rsh_no_tree_spawn 1 \
         -x OMP_NUM_THREADS \
         -x LD_LIBRARY_PATH \
         ../../build/gpmdcov input.in \
