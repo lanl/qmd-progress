92ed13bdf9bb7d24dd612c6034719539395b199f
Nicolas Bock <nicolasbock@gmail.com>
2017-04-25 05:51:53 -0600

    Make errors more verbose

--- examples/gpmdcov/run.sh
+++ examples/gpmdcov/run.sh
@@ -14,6 +14,7 @@ if ${MPIRUN} --version > /dev/null 2>&1; then
         --map-by ${MAP} \
         --hostfile ~/hostfile \
         --mca plm_rsh_no_tree_spawn 1 \
+        --mca orte_base_help_aggregate 0 \
         -x OMP_NUM_THREADS \
         -x LD_LIBRARY_PATH \
         ../../build/gpmdcov input.in \
