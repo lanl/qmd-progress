c78714b10cf28633f3afa1ba9bd0b1dc1b4fcc69
Nicolas Bock <nicolasbock@gmail.com>
2017-08-15 13:22:52 +0200

    Dump outputs for better debugging

--- examples/gpmd/run_test.sh
+++ examples/gpmd/run_test.sh
@@ -3,12 +3,12 @@
 # Script to run this test this program (GPMD).
 # Written by C. F. A. Negre Jul. 2016 Los Alamos Nat. Lab.
 
-set -e                                          # This will exit the script if there is any error
-MY_PATH=`pwd`                                   # Capturing the local path of the folder where we are running.
+set -e                               # This will exit the script if there is any error
+MY_PATH=$(readlink -f $(dirname $0)) # Capturing the local path of the folder where we are running.
 
-#RUN="mpirun -np 1 ../../build/gpmd"  #GPMD program 
-RUN="../../build/gpmd"  #GPMD program 
-cd ../latteTBparams/
+#RUN="mpirun -np 1 ../../build/gpmd"  #GPMD program
+RUN="${MY_PATH}/../../build/gpmd"  #GPMD program
+cd "${MY_PATH}/../latteTBparams/"
 PARAMS="$( pwd | sed 's_/_\\/_g' )"
 
 cd $MY_PATH
@@ -30,12 +30,14 @@ for name in ch4 sucrose ; do
   echo -e "\nTesting for "$name" \n"
   grep -A 20 DESCRIPTION $INFILE
 
-
   time $RUN input_tmp.in > out
   echo ""
   grep -e "Energy Total \[eV\] =" out | sed -e 's/Energy Total \[eV\]/ /g' | awk 'NF>1{print $2}' > energy.out
-#  python get_energy.py out > energy.out 
-  python test-energy.py --reference $REF --current energy.out --reltol 0.000001
+# python get_energy.py out > energy.out
+  python test-energy.py \
+      --reference $REF \
+      --current energy.out \
+      --reltol 0.000001
   rm $INFILE
   rm $REF
   rm coords_tmp.dat
--- examples/gpmd/test-energy.py
+++ examples/gpmd/test-energy.py
@@ -1,5 +1,26 @@
 #!/usr/bin/env python
 
+
+def read_energies(filename):
+    """Read a file and return a list of the read energies."""
+
+    energies = []
+    with open(filename) as fd:
+        for line in fd:
+            result = line.split()
+            energies.append(float(result[0]))
+    return energies
+
+
+def dump_file(filename):
+    """Print a file to screen."""
+
+    print("reading %s" % filename)
+    with open(filename) as fd:
+        for line in fd:
+            print(line.rstrip())
+
+
 def compare_MD(reference, current, reltol):
     """Compare MD energies.
 
@@ -8,57 +29,51 @@ def compare_MD(reference, current, reltol):
 
     """
 
-    import sys
-    #energy = re.compile("MD_data\s+([0-9eE.+-]+)\s+([0-9eE.+-]+)")
+    reference_energies = read_energies(reference)
+    current_energies = read_energies(current)
 
-    fd = open(reference)
-    reference_energies = []
-    for line in fd:
-        result = line.split()
-        reference_energies.append(float(result[0]))
-    fd.close()
-    
-    fd = open(current)
-    current_energies = []
-    for line in fd:
-        result = line.split()
-        current_energies.append(float(result[0]))
-    fd.close()
-
- 
     if len(reference_energies) != len(current_energies):
-        raise Exception("[error] different number of MD steps\n"
-                        + ("  reference ran for %4d steps\n" % (len(reference_energies)))
-                        + ("  current ran for   %4d steps\n" % (len(current_energies)))
-                        + "  can not compare")
+        dump_file(reference)
+        dump_file(current)
+        raise Exception(
+            ("[error] different number of MD steps\n" +
+             "  reference ran for %4d steps\n" +
+             "  current ran for   %4d steps\n" +
+             "  can not compare") %
+            (len(reference_energies), len(current_energies)))
 
     result = True
     for i in range(len(reference_energies)):
         diff = abs(reference_energies[i] - current_energies[i])
         if reference_energies[i] != 0:
-            diff = abs(diff/reference_energies[i])
+            diff = abs(diff / reference_energies[i])
         if diff > reltol:
-            print("failure in MD step %d" % (i+1))
+            print("failure in MD step %d" % (i + 1))
             result = False
     if not result:
-        raise Exception(("[error] when comparing '%s' with '%s'" % (reference, current))
-                        + "energies do not agree")
+        raise Exception(
+            ("[error] when comparing '%s' with '%s'" % (reference, current)) +
+            "energies do not agree")
 
-    print("Energy test passed without failure ...")  
+    print("Energy test passed without failure ...")
+
 
 def main():
     """The main function.
     """
 
-    import argparse, os, sys
-    
-    parser = argparse.ArgumentParser(description="""Script to compare MD results by using the total energy""")
+    import argparse
+
+    parser = argparse.ArgumentParser(
+        description="Script to compare MD results by using the total " +
+        "energy")
     parser.add_argument("--reference",
                         help="The reference output")
     parser.add_argument("--current",
                         help="The current output")
     parser.add_argument("--reltol",
-                        help="Relative tolerance when comparing, default is %(default)s",
+                        help="Relative tolerance when comparing, default " +
+                        "is %(default)s",
                         type=float,
                         default=1e-10)
     options = parser.parse_args()
