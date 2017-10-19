08d192f041488c3c6406551454032ca07c6ab666
Sue Mniszewski <smm@lanl.gov>
2017-10-19 10:46:31 -0600

    Took out debug writes.

--- src/prg_implicit_fermi_mod.F90
+++ src/prg_implicit_fermi_mod.F90
@@ -37,8 +37,6 @@ contains
   subroutine prg_implicit_fermi(h_bml, xi0_bml, p_bml, nsteps, nocc, &
     mu, beta, osteps, occErrLimit, threshold)
 
-!! check if all these matrices are needed
-
     implicit none
 
     type(bml_matrix_t), intent(in) :: h_bml
@@ -61,17 +59,6 @@ contains
     N = bml_get_N(h_bml)
     M = bml_get_M(h_bml)
 
-    !! Calculate from gershgorin bounds
-    allocate(gbnd(2))
-!    call bml_gershgorin(h_bml, gbnd)
-!    mu = 0.5_dp * (gbnd(2) + gbnd(1))
-! Test mu = 0
-!    mu = 0.0_dp
-
-!write(*,*)"mu=", mu, "h1=",gbnd(1), "hN=", gbnd(2)
-write(*,*)"mu=", mu
-!    deallocate(gbnd)
-
     allocate(trace(2))
 
     call bml_identity_matrix(bml_type, bml_element_real, dp, N, M, i_bml)
@@ -85,18 +72,15 @@ write(*,*)"mu=", mu
     firstTime = .false.
     iter = 0
     cnst = beta/(2**(2+nsteps))
-write(*,*)"beta =", beta, "cnst =", cnst
 
     do while ((osteps .eq. 0 .and. occErr .gt. occErrLimit) .or. &
               (osteps .gt. 0 .and. iter .lt. osteps))
       iter = iter + 1
-write(*,*)"do while iter=", iter
 
       ! Normalization 
       ! P0 = 0.5*II - cnst*(H0-mu0*II)
       call bml_copy(h_bml, p_bml)
       call prg_normalize_implicit_fermi(p_bml, cnst, mu) 
-write(*,*) "norm trace p =", bml_trace(p_bml)
 
       if (.NOT. bml_allocated(xi0_bml)) then
         call bml_zero_matrix(bml_type, bml_element_real, dp, N, M, xi0_bml)
@@ -107,18 +91,16 @@ write(*,*) "norm trace p =", bml_trace(p_bml)
 
       do i = 1, nsteps
         call bml_multiply_x2(p_bml, p2_bml, threshold, trace)
-write(*,*)"step ", i, " mul trace p =", bml_trace(p_bml), "trace p2 =", bml_trace(p2_bml)
+
         ! Y = 2*(P02-P0) + II
         call bml_copy(p2_bml, y_bml)
         call bml_add(y_bml, p_bml, 1.0_dp, -1.0_dp, threshold)
         call bml_add(y_bml, i_bml, 2.0_dp, 1.0_dp, threshold)
-write(*,*)"step ", i, " add trace y=", bml_trace(y_bml)
 
         !! First time do a full inverse
         if (firstTime .eqv. .true. ) then
           call bml_inverse(y_bml, xi_bml)
           firstTime = .false.
-          !firstTime = .true.
         else
           NrSchultz = 4
           do j = 1, NrSchultz
@@ -127,14 +109,12 @@ write(*,*)"step ", i, " add trace y=", bml_trace(y_bml)
             call bml_multiply(x_bml, xtmp_bml, xi_bml, 1.0_dp, 2.0_dp) 
           enddo
         endif
-write(*,*)"step ", i, " trace xi=", bml_trace(xi_bml)
 
         if (i == 1) then
           call bml_copy(xi_bml, xi0_bml)
         endif
 
         call bml_multiply(xi_bml, p2_bml, p_bml, 1.0_dp, 0.0_dp, threshold)
-write(*,*)"step ", i, " trace p=", bml_trace(p_bml)
       enddo
 
       trdPdmu = bml_trace(p_bml)
@@ -142,11 +122,8 @@ write(*,*)"step ", i, " trace p=", bml_trace(p_bml)
       trdPdmu = trdPdmu - bml_sum_squares(p_bml) ! sum p(i,j)**2
 
       trdPdmu = beta * trdPdmu
-write(*,*)"end of iter=", iter, "mu=", mu, "trP0=", trP0, "trdPdmu=", trdPdmu
       mu = mu + (nocc - trP0)/trdPdmu
       occErr = abs(trP0 - nocc)
-write(*,*)"end of iter=",iter, "mu=", mu, "occErr=", occErr, &
-      "occErrLimit=", occErrLimit
     enddo
 
     ! Adjust occupation
