ccc0f6fb8adb302f3dd3291afc94fdbe67d2f843
Christian F. A. Negre <christianfannegre@gmail.com>
2019-01-31 08:58:01 -0700

    Changed example_build back

--- example_build.sh
+++ example_build.sh
@@ -12,9 +12,9 @@ BML_LIB="$HOME/bml/install"
 MY_PATH=`pwd`
 
 # Configuring PROGRESS with OpenMP
-export CC=${CC:=xlC_r}
-export FC=${FC:=xlf90_r}
-export CXX=${CXX:=xlC_r}
+export CC=${CC:=gcc}
+export FC=${FC:=gfortran}
+export CXX=${CXX:=g++}
 export PROGRESS_OPENMP=${PROGRESS_OPENMP:=yes}
 export INSTALL_DIR=${INSTALL_DIR:="${MY_PATH}/install"}
 export PROGRESS_GRAPHLIB=${PROGRESS_GRAPHLIB:=no}
@@ -23,8 +23,8 @@ export CMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE:=Release}
 export PROGRESS_EXAMPLES=${PROGRESS_EXAMPLES:=yes}
 export PROGRESS_EXAMPLES=${PROGRESS_EXAMPLES:=yes}
 export CMAKE_PREFIX_PATH=${CMAKE_PREFIX_PATH:=$BML_LIB}
-export EXTRA_FCFLAGS=${EXTRA_FCFLAGS:="-I$BML_LIB/include"}
-export EXTRA_LINK_FLAGS=${EXTRA_LINK_FLAGS:="-L$BML_LIB/lib64/ -lbml_fortran -lbml"}
+export EXTRA_FCFLAGS=${EXTRA_FCFLAGS:=""}
+export EXTRA_LINK_FLAGS=${EXTRA_LINK_FLAGS:=""}
 ./build.sh configure
 
 # Make PROGRESS library and examples after running this script:
