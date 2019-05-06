aec0e4637c9a443b723e9be092dd4dbe4df111b9
cnegre <christianfannegre@gmail.com>
2019-05-06 15:17:00 -0400

    Changes in CmakeList.txt

--- example_build_ibm.sh
+++ example_build_ibm.sh
@@ -5,12 +5,9 @@
 rm -r build
 rm -r install
 
-BML_LIB="$MEMBERWORK/mat187/bml/install"
-#BML_LIB="/ccs/home/cnegre/bml/install/"
-MAGMA_PATH="${MEMBERWORK}/mat187/magma-2.5.0-orig/"
 MY_PATH=$(pwd)
-#export ESSL_DIR=${ESSL_DIR:=/sw/summitdev/essl/5.5.0-20161110}
-#export ESSL_DIR=${ESSL_DIR:=/sw/summitdev/essl/5.5.0-20161110}
+BML_LIB="${MY_PATH}/../bml/install"
+MAGMA_PATH="${MY_PATH}/../magma-2.5.0/"
 export ESSL_DIR=${ESSL_DIR:="${OLCF_ESSL_ROOT}"}
 export CC=${CC:=xlC_r}
 export FC=${FC:=xlf90_r}
@@ -23,10 +20,8 @@ export INSTALL_DIR=${INSTALL_DIR:="${MY_PATH}/install"}
 export PROGRESS_TESTING=${PROGRESS_TESTING:=yes}
 export PROGRESS_EXAMPLES=${PROGRESS_EXAMPLES:=yes}
 export CMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE:=Release}
-#export EXTRA_FCFLAGS=${EXTRA_FCFLAGS:="-I${BML_LIB}/include/ -I${MAGMA_PATH}/include -I${MAGMA_PATH}/lib"}
-#export EXTRA_LINK_FLAGS=${EXTRA_LINK_FLAGS:="-L${BML_LIB}/lib64/ -lbml_fortran -lbml  -L${OLCF_ESSL_ROOT}/lib64/ -qsmp=omp -lessl -lesslsmp -lesslsmpcuda -qextname -lxlopt -lxlf90_r -lxlfmath -lxl -lxlsmp -L/sw/summitdev/cuda/9.2.148/lib64/ -lcublas -lcudart -L${MAGMA_PATH}/lib/ -lmagma"}
-export EXTRA_LINK_FLAGS=${EXTRA_LINK_FLAGS:="-L${BML_LIB}/lib64/ -lbml_fortran -lbml  -L${OLCF_ESSL_ROOT}/lib64/ -qsmp=omp -lessl -lesslsmp -lesslsmpcuda -qextname -lxlopt -lxlf90_r -lxlfmath -lxl -lxlsmp -L/sw/summitdev/cuda/9.2.148/lib64/ -lcublas -lcudart"}
-export CMAKE_Fortran_FLAGS=${CMAKE_Fortran_FLAGS:="-I${BML_LIB}/include/ -qessl -qstrict=all -qsmp=omp -O2 -qextname -qxlf2003=polymorphic -qthreaded -I${BML_LIB}/include/  -I${MAGMA_PATH}/include "}
+export EXTRA_FCFLAGS=${EXTRA_FCFLAGS:="-I${BML_LIB}/include/ -qessl -qstrict=all -qsmp=omp -O2 -qextname -qxlf2003=polymorphic -qthreaded "}
+export EXTRA_LINK_FLAGS=${EXTRA_LINK_FLAGS:="-L${BML_LIB}/lib64/ -lbml_fortran -lbml -L${OLCF_ESSL_ROOT}/lib64/ -qsmp=omp -lessl -lesslsmp -lesslsmpcuda -qextname -lxlopt -lxlf90_r -lxlfmath -lxl -lxlsmp"}
 export CMAKE_PREFIX_PATH=${CMAKE_PREFIX_PATH:=$BML_LIB}
 
 
