fb7d82eba19f42805eec4db6cead67c6d028d1d8
Linnea Katarina Maria Andersson - 343840 <linneamk@darwin-fe2.lanl.gov>
2019-07-10 15:40:38 -0600

    testcg and read matrix update, also conjgrad update

--- examples/implicitfermi/main.F90
+++ examples/implicitfermi/main.F90
@@ -14,7 +14,12 @@ program main
   integer :: norb, mdim, verbose, nsteps,osteps 
   type(bml_matrix_t) :: ham_bml, p_bml, xi0_bml
   character(20) :: bml_type
-  real(dp) :: threshold, mu, beta, occErrLimit, nocc 
+  real(dp) :: threshold, mu, beta, occErrLimit, nocc  
+  integer :: i,j
+  real,allocatable :: vector(:)
+  !real :: bml_get 
+
+  
 
   !Some parameters that can be changed depending on the test.
   bml_type = "dense"
@@ -25,9 +30,11 @@ program main
   nsteps = 8
   osteps = 1
   occErrLimit = 1.0d-6
-  mu = 0
+  mu = 0.2
   beta = 0.25 
   nocc = 300
+
+  allocate(vector(384)) 
 
   call bml_zero_matrix(bml_type,bml_element_real,dp,norb,norb,ham_bml)
   
@@ -35,13 +42,21 @@ program main
   !which was precalculated with dftb+
   call bml_read_matrix(ham_bml,'hamiltonian_ortho.mtx')
   !call bml_print_matrix("ham",ham_bml,1,384,1,384) 
+  
+  open(1, file = 'ham384.txt', status = 'new')
+  do j=1,384
+        call bml_get_row(ham_bml,j,vector)
+        do i=1,384
+                write(1,*) vector(i)  
+        end do
+  end do 
 
   !Allocate the density matrix
   call bml_zero_matrix(bml_type,bml_element_real,dp,norb,norb,p_bml)
 
   call prg_implicit_fermi(ham_bml, xi0_bml, p_bml, nsteps, nocc,  mu, beta, osteps, occErrLimit, threshold)
 
-  call bml_print_matrix("density matrix",p_bml,1,384,1,384) 
+  !call bml_print_matrix("density matrix",p_bml,0,10,0,10) 
 
   call bml_deallocate(ham_bml)
   call bml_deallocate(p_bml)
