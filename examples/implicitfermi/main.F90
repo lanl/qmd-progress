e0ceb4d638e80dd50d48e13dfc2e71964e3c3168
linneamk <linnea.mk.andersson@gmail.com>
2019-07-12 17:07:51 -0600

    matrix is right yay

--- examples/implicitfermi/main.F90
+++ examples/implicitfermi/main.F90
@@ -15,8 +15,8 @@ program main
   type(bml_matrix_t) :: ham_bml, p_bml, xi0_bml
   character(20) :: bml_type
   real(dp) :: threshold, mu, beta, occErrLimit, nocc  
-  integer :: i,j
-  real,allocatable :: vector(:)
+  !integer :: i,j
+  !real,allocatable :: vector(:)
   !real :: bml_get 
 
   
@@ -26,20 +26,20 @@ program main
   threshold = 1.0d-9
   mdim = -1
   verbose = 1
-  norb = 674 
+  norb = 674
   nsteps = 10
   osteps = 1
-  occErrLimit = 1.0d-6
+  occErrLimit = 1.0
   mu = 0.2
-  beta = 0.25 
-  nocc = 300
+  beta = 4 
+  nocc = 200
 
   call bml_zero_matrix(bml_type,bml_element_real,dp,norb,norb,ham_bml)
   
   !The following Hamiltonian belongs to a water box structure
   !which was precalculated with dftb+
   call bml_read_matrix(ham_bml,'ham674.mtx')
-  !call bml_print_matrix("ham",ham_bml,1,384,1,384)   
+  call bml_print_matrix("ham",ham_bml,0,10,0,10)   
 
   !Allocate the density matrix
   call bml_zero_matrix(bml_type,bml_element_real,dp,norb,norb,p_bml)
