f3b8842644cdd9ed4b24e20abf5ec9a00eee383a
linneamk <linnea.mk.andersson@gmail.com>
2019-07-23 12:07:03 -0600

    final version

--- src/prg_implicit_fermi_mod.F90
+++ src/prg_implicit_fermi_mod.F90
@@ -17,6 +17,7 @@ module prg_implicit_fermi_mod
   integer, parameter :: dp = kind(1.0d0)
 
   public :: prg_implicit_fermi
+  public :: prg_test_density_matrix
 
 contains
 
@@ -49,8 +50,8 @@ contains
     real(dp) :: cg_tol
     real(dp), allocatable :: trace(:), gbnd(:)
     character(20) :: bml_type
-    integer :: N, M, i, iter
-    
+    integer :: N, M, i, iter, exp_order
+    logical :: direct
 
     bml_type = bml_get_type(h_bml)
     N = bml_get_N(h_bml)
@@ -67,7 +68,9 @@ allocate(trace(2))
     cg_tol = 0.000001 !Squared Frobenius norm of residual matrix 
     occErr = 1.0_dp
     iter = 0
-    cnst = beta/(2**(2+nsteps))
+    exp_order = 2**nsteps
+    cnst = beta/(4*exp_order)
+    direct = .true. 
 
     do while ((osteps .eq. 0 .and. occErr .gt. occErrLimit) .or. &
          (osteps .gt. 0 .and. iter .lt. osteps))
@@ -76,30 +79,37 @@ allocate(trace(2))
        ! Normalization 
        ! P0 = 0.5*II - cnst*(H0-mu0*II)
        call bml_copy(h_bml, p_bml)
-       call prg_normalize_implicit_fermi(p_bml, cnst, mu) 
+       call prg_normalize_implicit_fermi(p_bml, cnst, mu)
        
-       do i = 1, nsteps
-    
-          write(*,*) "i = ", i 
-          call bml_multiply_x2(p_bml, p2_bml, threshold, trace)
+       if (direct .eqv. .true.) then 
+          write(*,*) "Calling direct exp"
+          call prg_direct_exp(p_bml, y_bml, p2_bml, d_bml, wtmp_bml, exp_order, cg_tol, threshold)
+ 
+       else  
+       
+          do i = 1, nsteps
+       
+             write(*,*) "i = ", i 
+             call bml_multiply_x2(p_bml, p2_bml, threshold, trace)
 
-          ! Y = 2*(P2-P) + II
-          call bml_copy(p2_bml, y_bml)
-          call bml_add(y_bml, p_bml, 1.0_dp, -1.0_dp, threshold)
-          call bml_scale_add_identity(y_bml, 2.0_dp, 1.0_dp, threshold)
+             ! Y = 2*(P2-P) + II
+             call bml_copy(p2_bml, y_bml)
+             call bml_add(y_bml, p_bml, 1.0_dp, -1.0_dp, threshold)
+             call bml_scale_add_identity(y_bml, 2.0_dp, 1.0_dp, threshold)
 
-          ! Solve Y*P = P2
-          call prg_conjgrad(y_bml, p_bml, p2_bml, d_bml, wtmp_bml, cg_tol, threshold)
-           
-       enddo
+             ! Solve Y*P = P2
+             call prg_conjgrad(y_bml, p_bml, p2_bml, d_bml, wtmp_bml, cg_tol, threshold)
+              
+          enddo
+       end if 
 !       call bml_print_matrix("density matrix",p_bml,0,10,0,10)
-       trdPdmu = bml_trace(p_bml)
-       trP0 = trdPdmu
-       trdPdmu = trdPdmu - bml_sum_squares(p_bml) ! sum p(i,j)**2
+      ! trdPdmu = bml_trace(p_bml)
+      ! trP0 = trdPdmu
+      ! trdPdmu = trdPdmu - bml_sum_squares(p_bml) ! sum p(i,j)**2
 
-       trdPdmu = beta * trdPdmu
-       mu = mu + (nocc - trP0)/trdPdmu
-       occErr = abs(trP0 - nocc)
+      ! trdPdmu = beta * trdPdmu
+      ! mu = mu + (nocc - trP0)/trdPdmu
+      ! occErr = abs(trP0 - nocc)
     enddo
 
     ! Adjust occupation
@@ -119,6 +129,65 @@ allocate(trace(2))
     call bml_deallocate(y_bml)
 
   end subroutine prg_implicit_fermi
+
+  subroutine prg_direct_exp(p_bml, y_bml, p2_bml, d_bml, wtmp_bml, exp_order, cg_tol, threshold) 
+    
+    implicit none 
+
+    type(bml_matrix_t), intent(inout) :: p_bml, y_bml, p2_bml, d_bml, wtmp_bml
+    real(dp), intent(in) :: cg_tol, threshold 
+    integer, intent(in) :: exp_order
+    character(20) :: bml_type
+    integer :: N, M 
+    real(dp), allocatable :: trace(:)
+    type(bml_matrix_t) :: x_bml
+
+    allocate(trace(2))
+ 
+    bml_type = bml_get_type(p_bml)
+    N = bml_get_N(p_bml)
+    M = bml_get_M(p_bml) 
+
+    call bml_zero_matrix(bml_type, bml_element_real, dp, N, M, x_bml)
+
+    call bml_copy(p_bml, y_bml)
+    call bml_copy(p_bml, x_bml)
+    
+    call bml_scale_add_identity(y_bml, -1.0_dp, 1.0_dp, threshold)
+    
+    call prg_exp_by_squaring(y_bml, p2_bml, exp_order, threshold, trace)
+    call prg_exp_by_squaring(x_bml, p2_bml, exp_order, threshold, trace)  
+
+    call bml_add(y_bml, x_bml, 1.0_dp, 1.0_dp, threshold)
+ 
+    call prg_conjgrad(y_bml, p_bml, p2_bml, d_bml, wtmp_bml, cg_tol, threshold)        
+             
+    call bml_deallocate(x_bml)
+    deallocate(trace)
+
+  end subroutine prg_direct_exp
+
+  subroutine prg_exp_by_squaring(x_bml, x2_bml, exp_order, threshold, trace) 
+
+    implicit none
+
+    type(bml_matrix_t), intent(inout) :: x_bml, x2_bml
+    real(dp), intent(in) :: threshold
+    integer, intent(in) :: exp_order
+    real(dp), allocatable, intent(inout) :: trace(:)
+    integer :: k 
+
+    k = exp_order
+
+    do while (k.gt.1)
+        if (modulo(k,2) .eq. 0) then
+            call bml_multiply_x2(x_bml, x2_bml, threshold, trace)
+            k = k/2
+        end if
+        call bml_copy(x2_bml, x_bml)
+    enddo
+
+  end subroutine prg_exp_by_squaring
 
   subroutine prg_conjgrad(A_bml, p_bml, p2_bml, d_bml, wtmp_bml, cg_tol, threshold)
 
@@ -143,6 +212,7 @@ allocate(trace(2))
 
     do while (r_norm_new .gt. cg_tol) 
 
+      write(*,*) r_norm_new
       k = k + 1
       if (k .eq. 1) then 
         call bml_copy(p2_bml, d_bml)
@@ -157,7 +227,10 @@ allocate(trace(2))
       call bml_add(p2_bml, wtmp_bml, 1.0_dp, -alpha, threshold)
       r_norm_old = r_norm_new
       r_norm_new = bml_sum_squares(p2_bml)
-
+      if (k .gt. 100) then
+         write(*,*) "Conjugate gradient not converging"
+         stop
+      endif  
     enddo
 
   end subroutine prg_conjgrad
@@ -168,21 +241,22 @@ allocate(trace(2))
 
     type(bml_matrix_t), intent(in) :: ham_bml
     type(bml_matrix_t), intent(inout) :: p_bml
-    real(dp), intent(in) ::  beta, mu
+    real(dp), intent(in) ::  beta, mu, threshold
     character(20) :: bml_type
-    integer :: N, M
+    integer :: N, i
     real(dp), allocatable ::  eigenvalues(:)
     type(bml_matrix_t) :: eigenvectors_bml,occupation_bml,aux_bml,aux1_bml
 
     bml_type = bml_get_type(p_bml)
     N = bml_get_N(p_bml)
-    M = bml_get_M(p_bml)
   
     allocate(eigenvalues(N))
     
-    call bml_zero_matrix(bml_type,bml_element_real,dp,N,M,eigenvectors_bml)
+    call bml_zero_matrix(bml_type,bml_element_real,dp,N,N,eigenvectors_bml)
 
     call bml_diagonalize(ham_bml,eigenvalues,eigenvectors_bml)
+
+    !call bml_print_matrix("eigenvectors_bml",eigenvectors_bml,0,10,0,10)
 
     do i=1,N   
        eigenvalues(i) = fermi(eigenvalues(i),mu,beta)
@@ -191,13 +265,17 @@ allocate(trace(2))
     call bml_zero_matrix(bml_type,bml_element_real,dp,N,N,occupation_bml)
     call bml_set_diagonal(occupation_bml, eigenvalues) 
 
-    call bml_zero_matrix(bml_type,bml_element_real,dp,norb,norb,aux_bml)
+    !call bml_print_matrix("occupation",occupation_bml,650,660,650,660)
+
+    call bml_zero_matrix(bml_type,bml_element_real,dp,N,N,aux_bml)
     call bml_multiply(eigenvectors_bml, occupation_bml, aux_bml, 1.0_dp, 0.0_dp,threshold)
     call bml_deallocate(occupation_bml)
 
     call bml_zero_matrix(bml_type,bml_element_real,dp,N,N,aux1_bml)
     call bml_transpose(eigenvectors_bml, aux1_bml)
     call bml_deallocate(eigenvectors_bml)
+
+    !call bml_print_matrix("eigenvectors transposed",aux1_bml,0,10,0,10)
 
     call bml_multiply(aux_bml, aux1_bml, p_bml, 1.0_dp, 0.0_dp, threshold)
     call bml_deallocate(aux_bml)
@@ -207,6 +285,16 @@ allocate(trace(2))
 
   end subroutine 
 
+  !> Gives the Fermi distribution value for energy e.
+  !! \param e Energy.
+  !! \param mu Fermi energy.
+  !!
+  real(dp) function fermi(e,mu,beta)
 
+    real(dp), intent(in) :: e, mu, beta
+
+    fermi = 1.0_dp/(1.0_dp+exp(beta*(e-mu)))
+
+  end function fermi
 
 end module prg_implicit_fermi_mod
