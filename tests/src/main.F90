00ec7a80d2c582f3d8fb2aa4d8c0c71c81260d7c
linneamk <linnea.mk.andersson@gmail.com>
2019-07-24 13:34:41 -0600

    uncommented occupation update, changed test, indented

--- tests/src/main.F90
+++ tests/src/main.F90
@@ -158,10 +158,11 @@ program main
   case("prg_implicit_fermi") !Use implicit recursive expansion by Niklasson to build density matrix
      write(*,*) "Testing the construction of the density matrix at KbT > 0 and at mu = Ef from implicit_fermi_mod"
      mu = 0.2_dp
-     beta = 4.0_dp
+     beta = 4.0_dp !nocc,osteps,occerrlimit
      call bml_zero_matrix(bml_type,bml_element_real,dp,norb,norb,rho1_bml)
      call prg_implicit_fermi(ham_bml, rho1_bml, 10, 10.0_dp, mu, beta, 1, 1.0_dp, threshold)
-     call prg_test_density_matrix(ham_bml, rho_bml, beta, mu, threshold)
+     mu = 0.2_dp
+     call prg_test_density_matrix(ham_bml, rho_bml, beta, mu, 10.0_dp, 1, 1.0_dp, threshold)
      call bml_add(rho1_bml,rho_bml,1.0_dp,-1.0_dp)
      error_calc = bml_fnorm(rho1_bml) 
      if(error_calc.gt.0.1_dp)then
