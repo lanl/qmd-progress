d91428f73db47a63fbbd42ad441029d7c82eec98
Nicolas Bock <nicolasbock@gmail.com>
2019-08-21 06:56:46 -0600

    Remove hardcoded compiler versions
    Signed-off-by: Nicolas Bock <nicolasbock@gmail.com>

--- scripts/install-bml.sh
+++ scripts/install-bml.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-set -ev
+set -e -x
 
 cd ~
-git clone --depth=1 https://github.com/lanl/bml.git;
+git clone --depth=1 https://github.com/lanl/bml.git
+
 cd bml
-CC=gcc-6 FC=gfortran-6 CXX=g++-6 \
-  CMAKE_BUILD_TYPE=Release BLAS_VENDOR=GNU \
-  BML_OPENMP=yes BML_TESTING=no \
+CMAKE_BUILD_TYPE=Release \
+  BLAS_VENDOR=GNU \
+  BML_OPENMP=yes \
+  BML_TESTING=no \
   ./build.sh install
