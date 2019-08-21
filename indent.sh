88dfeefe60d86e6bcec1d74cb05f6fe4363f39f1
Nicolas Bock <nicolasbock@gmail.com>
2019-08-21 10:12:56 -0600

    Update indent script
    Copy the indent script from bml to ensure the coding styles are the
    same between these two projects.
    
    Signed-off-by: Nicolas Bock <nicolasbock@gmail.com>

--- indent.sh
+++ indent.sh
@@ -1,22 +1,54 @@
 #!/bin/bash
-#
-# Install emacs: 
-#	  sudo apt-get install emacs
-#
-# Get the indentation emacs script as follows: 
-#  	wget https://raw.github.com/Glavin001/atom-beautify/master/src/beautifiers/fortran-beautifier/emacs-fortran-formating-script.lisp
-# 
-# Change to downcase: 
-#   sed 's/-upcase-/-downcase-/g' emacs-fortran-formating-script.lisp > emacs-fortran-formating-script-down.lisp
-#
-# To use it just call this script from the main qmd-progress folder. 
-# 	./tools/indent.sh
 
-PROGRESS_PATH=$(dirname $(readlink -f $0))
+set -x
 
-for file in $PROGRESS_PATH/src/*.F90
-do 
-  emacs -batch -l $PROGRESS_PATH/emacs-fortran-formating-script-down.lisp -f f90-batch-indent-region $file
-done	
+: ${EMACS:=$(command -v emacs)}
+: ${INDENT_ARGS:="-gnu -nut -i4 -bli0 -cli4 -ppi0 -cbi0 -npcs -bfda"}
 
+declare -a C_FILES
+declare -a FORTRAN_FILES
 
+if ! git status > /dev/null; then
+  cat <<EOF
+We are not inside a git repository. Please specify the files to indent
+manually.
+EOF
+  exit 1
+fi
+
+if (( $# > 0 )); then
+  for file in "$@"; do
+    case "${file##*.}" in
+      "c" | "h")
+        C_FILES[${#C_FILES[@]}]="$file"
+        ;;
+      "F90")
+        FORTRAN_FILES[${#FORTRAN_FILES[@]}]="$file"
+        ;;
+      *)
+        echo "unknown suffix"
+        ;;
+    esac
+  done
+else
+  readarray -t C_FILES < <(git ls-files -- *.c *.h)
+  readarray -t FORTRAN_FILES < <(git ls-files -- *.F90)
+fi
+
+for f in "${C_FILES[@]}" "${FORTRAN_FILES[@]}"; do
+  sed -i -e 's:\s\+$::' "${f}"
+done
+
+for f in "${C_FILES[@]}"; do
+  indent ${INDENT_ARGS} "${f}"
+done
+
+for f in "${FORTRAN_FILES[@]}"; do
+  ${EMACS} --batch \
+    "${f}" \
+    --eval "(setq f90-do-indent 2)" \
+    --eval "(setq f90-if-indent 2)" \
+    --eval "(setq f90-type-indent 2)" \
+    --eval "(indent-region (minibuffer-prompt-end) (point-max) nil)" \
+    -f save-buffer
+done
