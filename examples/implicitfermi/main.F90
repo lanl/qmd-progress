196a63ed24d28d6e92935ab52aeb4bed672e147b
linneamk <linnea.mk.andersson@gmail.com>
2019-08-30 15:12:54 -0600

    Added comments

--- examples/implicitfermi/main.F90
+++ examples/implicitfermi/main.F90
@@ -14,12 +14,8 @@ program main
   integer :: norb, mdim, verbose, nsteps,osteps 
   type(bml_matrix_t) :: ham_bml, p_bml, p1_bml
   character(20) :: bml_type
-  real(dp) :: threshold, mu, beta, occErrLimit, nocc, error  
-  !integer :: i,j
-  !real,allocatable :: vector(:)
-  !real :: bml_get 
-
-  
+  real(dp) :: threshold, mu, beta, occErrLimit, nocc, error, tol  
+  integer :: k, method 
 
   !Some parameters that can be changed depending on the test.
   bml_type = "ellpack"
@@ -28,16 +24,17 @@ program main
   verbose = 1
   norb = 384
   nsteps = 10
-  osteps = 1
+  osteps = 10
   occErrLimit = 1.0
-  mu = 0.0
+  mu = 10.0
   beta = 100 
-  nocc = 200
+  nocc = 384
+  k = 2
+  method = 0
+  tol = 10e-8
 
   call bml_zero_matrix(bml_type,bml_element_real,dp,norb,norb,ham_bml)
   
-  !The following Hamiltonian belongs to a water box structure
-  !which was precalculated with dftb+
   call bml_read_matrix(ham_bml,'hamiltonian_ortho.mtx')
   call bml_print_matrix("hamiltonian",ham_bml,0,10,0,10)   
 
@@ -45,11 +42,11 @@ program main
   call bml_zero_matrix(bml_type,bml_element_real,dp,norb,norb,p_bml)
   call bml_zero_matrix(bml_type,bml_element_real,dp,norb,norb,p1_bml)
 
-  call prg_implicit_fermi(ham_bml, p_bml, nsteps, nocc,  mu, beta, osteps, occErrLimit, threshold)
+  call prg_implicit_fermi(ham_bml, p_bml, nsteps, k, nocc,  mu, beta, method, osteps, occErrLimit, threshold, tol)
 
   call bml_print_matrix("density matrix",p_bml,0,10,0,10) 
   write(*,*) "mu = ", mu 
-  mu = 0.0
+  mu = 10.0
   call prg_test_density_matrix(ham_bml, p1_bml, beta, mu, nocc, osteps, occErrLimit, threshold)
 
   call bml_print_matrix("density matrix",p1_bml,0,10,0,10)
