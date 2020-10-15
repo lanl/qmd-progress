9d2c6dd88b58233a2703883111c9063d34b1b830
Christian Francisco Andres Negre - 295454 <cnegre@lanl.gov>
2020-10-14 21:30:23 -0600

    sp2tc compiling

--- example_build_tcsp2.sh
+++ example_build_tcsp2.sh
@@ -14,18 +14,18 @@ MY_PATH=`pwd`
 # Configuring PROGRESS with OpenMP
 export CC=${CC:=gcc}
 export FC=${FC:=gfortran}
-export CXX=${CXX:=nvcc}
+export CXX=${CXX:=g++}
 export PROGRESS_OPENMP=${PROGRESS_OPENMP:=yes}
 export INSTALL_DIR=${INSTALL_DIR:="${MY_PATH}/install"}
 export PROGRESS_GRAPHLIB=${PROGRESS_GRAPHLIB:=no}
 export PROGRESS_TCSP2=${PROGRESS_TCSP2:=yes}
-export PROGRESS_TESTING=${PROGRESS_TESTING:=yes}
+export PROGRESS_TESTING=${PROGRESS_TESTING:=no}
 export CMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE:=Release}
-export PROGRESS_EXAMPLES=${PROGRESS_EXAMPLES:=yes}
+export PROGRESS_EXAMPLES=${PROGRESS_EXAMPLES:=no}
 export CMAKE_PREFIX_PATH=${CMAKE_PREFIX_PATH:=$BML_LIB}
 export EXTRA_FCFLAGS=${EXTRA_FCFLAGS:=""}
 export EXTRA_LINK_FLAGS=${EXTRA_LINK_FLAGS:=""}
-./build.sh configure
+./build.sh configure 
 
 # Make PROGRESS library and examples after running this script:
 #   cd build
