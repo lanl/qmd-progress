48bdeaaa309cbcb406948e349d69e82f54209ba0
cnegre <christianfannegre@gmail.com>
2021-01-07 19:35:44 -0700

    Added seed

--- src/prg_modelham_mod.F90
+++ src/prg_modelham_mod.F90
@@ -118,20 +118,24 @@ contains
   subroutine prg_twolevel_model(ea, eb, dab, daiaj, dbibj, dec, rcoeff, reshuffle, &
    & seed, h_bml, verbose)
     real(dp), intent(in) :: ea, eb, dab, daiaj, dbibj, rcoeff
-    integer, intent(in) :: verbose
-    integer :: seed
+    integer, intent(in) :: verbose, seed
+    integer, allocatable :: seedin(:)
     logical, intent(in) :: reshuffle
     type(bml_matrix_t),intent(inout) :: h_bml
     real(dp), allocatable :: diagonal(:), row(:), rowi(:), rowj(:)
     type(bml_matrix_t) :: ht_bml
-    integer :: norbs, i, j
+    integer :: norbs, i, j, ssize
     real(dp) :: dec, dist, ran
     
     norbs = bml_get_N(h_bml)
     allocate(diagonal(norbs))
     allocate(row(norbs))
-    
-    call random_seed(seed)
+
+    call random_seed()
+    call random_seed(size=ssize)
+    allocate(seedin(ssize))
+    seedin = seed    
+    call random_seed(PUT=seedin)
         
     do i=1,norbs
       if(mod(i,2) == 0)then
