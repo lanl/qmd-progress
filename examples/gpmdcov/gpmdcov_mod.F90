2bad4a5d37deed89690be4a511e1a06f0d291723
Christian Francisco Andres Negre - 295454 <cnegre@lanl.gov>
2021-02-11 17:24:09 -0700

    Added changes to gpmdcov

--- examples/gpmdcov/gpmdcov_mod.F90
+++ examples/gpmdcov/gpmdcov_mod.F90
@@ -37,48 +37,52 @@ contains
     integer, optional, intent(in) :: verbose
 
 
-    norbs = size(evals, dim = 1)    
-    
+    norbs = size(evals, dim = 1)
+
     mu0 = mu
-
-
-    do j = 1, norbs
-                fvals(j) = 1.0_dp/(exp(beta*(evals(j)-mu0))+1.0_dp)
-    end do
+    write(*,*)"NOCC",nocc, norbs, mu0
 
     do j = 1, norbs
-                occ = occ + fvals(j)*dvals(j)
+      fvals(j) = 1.0_dp/(exp(beta*(evals(j)-mu0))+1.0_dp)
     end do
-    
+
+    occ = 0.0_dp
+    do j = 1, norbs
+      !                write(*,*)"fvals, dvals",j,fvals(j),dvals(j), evals(j)
+      occ = occ + fvals(j)*dvals(j)
+    end do
+
+    !stop
     occErr = abs(occ-nocc)
 
-    if (present(verbose)) then 
-        if (verbose >= 1) write(*,*)"In gpmdcov_musearch ..."
+    if (present(verbose)) then
+      if (verbose >= 1) write(*,*)"In gpmdcov_musearch ..."
     endif
 
     do i = 1, maxMuIter
-        do j = 1, norbs
-                fvals(j) = 1.0_dp/(exp(beta*(evals(j)-mu0))+1.0_dp)
-        end do
-    
-        occ = 0.0_dp
-        den = 0.0_dp
-        do j = 1, norbs
-                occ = occ + fvals(j)*dvals(j)
-                den = den + beta*fvals(j)*(1-fvals(j))*dvals(j)
-        end do
+      do j = 1, norbs
+        fvals(j) = 1.0_dp/(exp(beta*(evals(j)-mu0))+1.0_dp)
+      end do
 
-        occErr = abs(occ-nocc)
-        mu0 = mu0 + (nocc - occ)/den
-        mu = mu0
+      occ = 0.0_dp
+      den = 0.0_dp
+      do j = 1, norbs
+        occ = occ + fvals(j)*dvals(j)
+        den = den + beta*fvals(j)*(1.0_dp-fvals(j))*dvals(j)
+      end do
 
-        if (i .eq. maxMuIter) then 
-                write(*,*) "Max mu iters reached"
-                stop
-        else
-                if(occErr .lt. occTol) exit
-        end if
-    
+      occErr = abs(occ - nocc)
+      mu0 = mu0 + (nocc - occ)/den
+      mu = mu0
+
+      write(*,*)"occ, occErr, nocc, den, mu", occ, occErr, nocc, den, mu
+      if (i .eq. maxMuIter) then
+        write(*,*) "Max mu iters reached"
+        stop
+      else
+        if(occErr .lt. occTol) exit
+      end if
+
     end do
 
   end subroutine gpmdcov_musearch
