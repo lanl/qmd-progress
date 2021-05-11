3551bc9c2b1804d1474bb0e5988d6fe573d0d927
Christian Francisco Andres Negre - 295454 <cnegre@lanl.gov>
2021-05-11 12:52:07 -0600

    Kernel

--- examples/gpmdcov/gpmdcov_kernel.F90
+++ examples/gpmdcov/gpmdcov_kernel.F90
@@ -22,14 +22,18 @@ contains
     use prg_response_mod
     real(dp), allocatable :: chargePertVect(:)
     real(dp), allocatable :: coul_pot(:)
-    real(dp), allocatable :: Jacob(:)
+    real(dp), allocatable :: Jacob(:), lwork(:), work(:)
     real(dp) :: mlsi
-    integer :: nats
+    integer :: nats, l, m, lm, info
+    integer, allocatable :: ipiv(:)
+    type(system_type), allocatable    :: mysyprt(:)
 
     if(.not. allocated(chargePertVect))allocate(chargePertVect(nats))
     if(.not. allocated(coul_pot))allocate(coul_pot(nats))
 
     allocate(Jacob(nats*nats))
+    allocate(mysyprt(gpat%TotalParts))
+
     do i=1,nats
       write(*,*)"Constructing response for atom =",i
       chargePertVect=0.0_dp
@@ -46,7 +50,6 @@ contains
       coul_pot = coul_pot_k + coul_pot_r
 
       !#ifdef DO_MPI
-      !    !do ipt= gpat%localPartMin(myRank), gpat%localPartMax(myRank)
       do iptt=1,partsInEachRank(myRank)
         ipt= reshuffle(iptt,myRank)
         !#else
@@ -54,70 +57,100 @@ contains
         !#endif
         norb = syprt(ipt)%estr%norbs
 
-        if(.not.allocated(syprt(ipt)%estr%coul_pot_k))then
-          allocate(syprt(ipt)%estr%coul_pot_k(syprt(ipt)%nats))
-          allocate(syprt(ipt)%estr%coul_pot_r(syprt(ipt)%nats))
+        if(.not.allocated(mysyprt(ipt)%estr%coul_pot_k))then
+          allocate(mysyprt(ipt)%estr%coul_pot_k(syprt(ipt)%nats))
+          allocate(mysyprt(ipt)%estr%coul_pot_r(syprt(ipt)%nats))
+          allocate(mysyprt(ipt)%net_charge(syprt(ipt)%nats))
         endif
 
-        syprt(ipt)%estr%coul_pot_k = 0.0_dp
-        syprt(ipt)%estr%coul_pot_r = 0.0_dp
-        syprt(ipt)%net_charge = 0.0_dp
+        mysyprt(ipt)%estr%coul_pot_k = 0.0_dp
+        mysyprt(ipt)%estr%coul_pot_r = 0.0_dp
+        mysyprt(ipt)%net_charge = 0.0_dp
 
         mlsi = mls()
         !> Get Coulombic potential and charges for the part.
         do j=1,gpat%sgraph(ipt)%lsize
           jj = gpat%sgraph(ipt)%core_halo_index(j)+1
-          syprt(ipt)%estr%coul_pot_k(j) = coul_pot_k(jj)
-          syprt(ipt)%estr%coul_pot_r(j) = coul_pot_r(jj)
-          syprt(ipt)%net_charge(j) = chargePertVect(jj)
+          mysyprt(ipt)%estr%coul_pot_k(j) = coul_pot_k(jj)
+          mysyprt(ipt)%estr%coul_pot_r(j) = coul_pot_r(jj)
+          mysyprt(ipt)%net_charge(j) = chargePertVect(jj)
         enddo
         write(*,*)"Time for coulomb =",mls() - mlsi
-
-        !Get part(j)\%hamPert given syprt(j)\%over, and syprt(j)\%pot
-        call bml_zero_matrix(lt%bml_type,bml_element_real,dp,norb,norb,syprt(ipt)%estr%ham)
-        call bml_zero_matrix(lt%bml_type,bml_element_real,dp,norb,norb,syprt(ipt)%estr%mat)
-        call bml_copy_new(syprt(ipt)%estr%mat,syprt(ipt)%estr%ham)
+        !Get part(j)\%hamPert given mysyprt(j)\%over, and mysyprt(j)\%pot
+        call bml_zero_matrix(lt%bml_type,bml_element_real,dp,norb,norb,mysyprt(ipt)%estr%ham)
+        call bml_zero_matrix(lt%bml_type,bml_element_real,dp,norb,norb,mysyprt(ipt)%estr%mat)
+        call bml_copy_new(mysyprt(ipt)%estr%mat,mysyprt(ipt)%estr%ham)
 
         mlsi = mls()
         call gpmdcov_msI("gpmdcov_getKernel","Entering prg_get_hscf to construct perturbative ham ...",lt%verbose,myRank)
-        call prg_get_hscf(syprt(ipt)%estr%mat,syprt(ipt)%estr%over,syprt(ipt)%estr%ham,syprt(ipt)%spindex,&
-             syprt(ipt)%estr%hindex,tb%hubbardu,syprt(ipt)%net_charge,&
-             syprt(ipt)%estr%coul_pot_r,syprt(ipt)%estr%coul_pot_k,lt%mdim,lt%threshold)
+        call prg_get_hscf(mysyprt(ipt)%estr%mat,syprt(ipt)%estr%over,mysyprt(ipt)%estr%ham,syprt(ipt)%spindex,&
+             syprt(ipt)%estr%hindex,tb%hubbardu,mysyprt(ipt)%net_charge,&
+             mysyprt(ipt)%estr%coul_pot_r,mysyprt(ipt)%estr%coul_pot_k,lt%mdim,lt%threshold)
         write(*,*)"Time for hscf construction =",mls() - mlsi
 
-        call prg_orthogonalize(syprt(ipt)%estr%ham,syprt(ipt)%estr%zmat,syprt(ipt)%estr%oham,&
-                & lt%threshold,lt%bml_type,lt%verbose)
+        call prg_orthogonalize(mysyprt(ipt)%estr%ham,syprt(ipt)%estr%zmat,mysyprt(ipt)%estr%oham,&
+             & lt%threshold,lt%bml_type,lt%verbose)
 
-        call prg_toEigenspace(syprt(ipt)%estr%oham,syprt(ipt)%estr%ham,syprt(ipt)%estr%evects,lt%threshold,lt%verbose)
- !       call bml_print_matrix("hamPert",syprt(ipt)%estr%ham,0,10,0,10)
-
+        call prg_toEigenspace(mysyprt(ipt)%estr%oham,mysyprt(ipt)%estr%ham,syprt(ipt)%estr%evects,lt%threshold,lt%verbose)
+        !call bml_print_matrix("hamPert",mysyprt(ipt)%estr%ham,0,10,0,10)
         mlsi = mls()
-        call prg_canon_response(syprt(ipt)%estr%mat,syprt(ipt)%estr%ham,nocc,beta,syprt(ipt)%estr%evects,&
-                &syprt(ipt)%estr%aux(1,:),ef,12,norb)
+        call prg_canon_response(mysyprt(ipt)%estr%mat,mysyprt(ipt)%estr%ham,nocc,beta,syprt(ipt)%estr%evects,&
+             &syprt(ipt)%estr%aux(1,:),ef,12,norb)
         write(*,*)"Time for hscf canon =",mls() - mlsi
 
-        call prg_get_charges(syprt(ipt)%estr%mat, syprt(ipt)%estr%over, syprt(ipt)%estr%hindex, syprt(ipt)%net_charge, tb%numel,&
-         syprt(ipt)%spindex, lt%mdim, lt%threshold)
+        call prg_get_charges(mysyprt(ipt)%estr%mat, syprt(ipt)%estr%over, syprt(ipt)%estr%hindex, mysyprt(ipt)%net_charge, tb%numel,&
+             syprt(ipt)%spindex, lt%mdim, lt%threshold)
 
         !Constructing 1D matrix J to prepare for all-gather
         do j=1,gpat%sgraph(ipt)%llsize
           jj = gpat%sgraph(ipt)%core_halo_index(j)+1
-          Jacob(i*(nats - 1) + jj) = syprt(ipt)%net_charge(j)
+          Jacob(i*(nats - 1) + jj) = mysyprt(ipt)%net_charge(j)
         enddo
 
       enddo
 
+
+
     enddo
 
+
+    do iptt=1,partsInEachRank(myRank)
+      ipt= reshuffle(iptt,myRank)
+      deallocate(mysyprt(ipt)%estr%coul_pot_k)
+      deallocate(mysyprt(ipt)%estr%coul_pot_r)
+      deallocate(mysyprt(ipt)%net_charge)
+    enddo
+
+    deallocate(mysyprt)
+
 #ifdef DO_MPI
-  if (getNRanks() .gt. 1) then
-    call prg_sumRealReduceN(Jacob, nats*nats)
-  endif
+    if (getNRanks() .gt. 1) then
+      call prg_sumRealReduceN(Jacob, nats*nats)
+    endif
 #endif
 
-   write(*,*)Jacob
-      call prg_wait
-      stop
+    call prg_wait
+
+    if(.not. allocated(Ker))then
+      allocate(Ker(nats,nats))
+    endif
+
+    lm = 0
+    do l=1,nats
+      do m=1,nats
+        lm = lm + 1
+        Ker(l,m) = Jacob(lm)
+        if(l == m) Ker(l,m) = Ker(l,m) - 1.0_dp
+      enddo
+    enddo
+ 
+    deallocate(Jacob)
+
+    allocate(work(nats+nats*nats))
+    allocate(ipiv(nats))
+
+    call DGETRF(nats, nats, Ker, nats, ipiv, info)
+    call DGETRI(nats, Ker, nats, ipiv, work, nats+nats*nats, info)
 
   end subroutine gpmdcov_getKernel
 
