90f07540c60849495886dcec6cef49de62f9f95c
Joshua Finkelstein <finkeljo@pn2008497.lanl.gov>
2022-05-17 15:04:20 -0600

    example tcsp2 build script

--- scripts/example_build_tcsp2.sh
+++ scripts/example_build_tcsp2.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-# Make a copy of this script and modify the METIS and BML library locations
-
+# Make a copy of this script and modify the PROGRESS_SP2TC flag as needed.
+# Tested on V100 and A100 GPUs. See https://doi.org/10.1021/acs.jctc.1c00057
+# for details.
 rm -r build
 rm -r install
 
@@ -18,16 +19,11 @@ export CXX=${CXX:=g++}
 export PROGRESS_OPENMP=${PROGRESS_OPENMP:=yes}
 export INSTALL_DIR=${INSTALL_DIR:="${MY_PATH}/install"}
 export PROGRESS_GRAPHLIB=${PROGRESS_GRAPHLIB:=no}
-export PROGRESS_SP2TC=${PROGRESS_SP2TC:=Fortran}
-#export PROGRESS_SP2TC=${PROGRESS_SP2TC:=C++}
+export PROGRESS_SP2TC=${PROGRESS_SP2TC:=Fortran} # Choose Fortran or C++
 export PROGRESS_TESTING=${PROGRESS_TESTING:=no}
-#export CMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE:=Debug}
 export PROGRESS_EXAMPLES=${PROGRESS_EXAMPLES:=yes}
 export CMAKE_PREFIX_PATH=${CMAKE_PREFIX_PATH:=$BML_LIB}
 export EXTRA_FCFLAGS=${EXTRA_FCFLAGS:="-llapack -lblas -fopenmp"}
 export EXTRA_LINK_FLAGS=${EXTRA_LINK_FLAGS:="-llapack -lblas -fopenmp"}
 ./build.sh configure 
 
-# Make PROGRESS library and examples after running this script:
-#   cd build
-#   make
