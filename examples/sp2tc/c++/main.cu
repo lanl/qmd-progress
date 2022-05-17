20197b788d3d0777e976d1fa0ad6a6a526ef2522
Joshua David Finkelstein - 350854 <finkeljos@gmail.com>
2022-05-17 12:56:27 -0600

    clean up c++ tensor core example

--- examples/sp2tc/c++/main.cu
+++ examples/sp2tc/c++/main.cu
@@ -3,20 +3,15 @@
 #include <stdio.h>
 #include <math.h>
 #include <fstream>
-#include <regex>
 #include <typeinfo>
 #include <cuda.h>
 #include <cublas_v2.h>
 #include <cuda_fp16.h>
-#include <random>
-#include <ctime>
 #include <cmath>
 #include <vector>
-#include <chrono>
 #include "tcore_hp_emulator.cuh"
 #include "linalg_tools.cuh"
 #include "prg_sp2_tensorcore.cuh"
-
 
 
 void produce_hamiltonian (const unsigned N, float *X) {
@@ -36,46 +31,33 @@ int main(int argc, char *argv[])
     size_t N = atoi(argv[1]);
     size_t Nocc = atoi(argv[2]);
 
-    int Stopp = 0;
     float eps = 1e-16;
-
 
     std::vector<float> Idemp_Error;
     
-    std::cout << "Mat Size: " << N << std::endl;
+    std::cout << "Matrix Size: " << N << std::endl;
     std::cout << "Occupied orbitals: " << Nocc << std::endl;
 
     // Set GPU
     int device = 0;
     cudaSetDevice(device);
 
-    // Cublas Handle
-    cublasHandle_t handle;
-    cublasCreate(&handle);
-    
-
-    cublasStatus_t cublasStat = cublasSetMathMode(handle, CUBLAS_TENSOR_OP_MATH);
-
+    // Allocate memory
     float *H;
     H = (float*) malloc(N*N*sizeof(float));
     double *D; 
     D = (double*) malloc(N*N*sizeof(double));
     
-
-    // Produce Hamiltonian and Identity matrix 
+    // Produce Hamiltonian 
     produce_hamiltonian(N,H);
-    
-    // Get device id
-    cudaGetDevice(&device); 
 
-    float idemtol=1e-16;
+    float idemtol=1e-6;
     char sp2conv,prec;
     int verbose=0;
-    prec='s';
     float bndfil=float(Nocc)/float(N);
-    for (int i=0;i<10000;i++){
-        prg_sp2_tensorcore(N,H,D,eps,bndfil,1,1000,sp2conv,idemtol,verbose);
-    }
+    
+    prg_sp2_tensorcore(N,H,D,eps,bndfil,1,1000,sp2conv,idemtol,verbose);
+ 
     std::cout << D[0] << std::endl; 
 }
 
