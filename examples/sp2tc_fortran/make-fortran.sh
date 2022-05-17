478992c3cf8d9c006ec830f0ef89649b1dce2951
Joshua David Finkelstein - 350854 <finkeljos@gmail.com>
2022-05-17 11:22:59 -0600

    clean up tcsp2 examples

--- examples/sp2tc_fortran/make-fortran.sh
+++
@@ -1,1 +0,0 @@
-gfortran   -I$HOME/bml/install/include  -I$HOME/qmd-progress/install/include -g -o main main.F90 -L$HOME/qmd-progress/install/lib64/ -lprogress -lprg_sp2_tc_fortran -L/usr/local/cuda-11.2/lib64 -L/$CUDA_LIB -lcudart -lcuda -lcublas -lstdc++
