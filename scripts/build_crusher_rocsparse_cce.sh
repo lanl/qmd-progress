1a8ef11aba53420b003a15c769952f7e9b8ecb77
Mike Wall <mewall@lanl.gov>
2022-07-20 15:31:42 -0400

    Modifications to enable offload build on crusher
      o CMakelists.txt mods to recognize Cray compilers
      o Support rocSPARSE build for ellpack

---
+++ scripts/build_crusher_rocsparse_cce.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+rm -r build
+rm -r install
+
+# Set BML Library location
+MY_PATH=`pwd`
+export BML_DIR=${MY_PATH}/../bml/install
+# Configuring PROGRESS with OpenMP
+#export MAGMA_PATH=${MAGMA_PATH:=${OLCF_MAGMA_ROOT}}
+export CC=${CC:=cc}
+export FC=${FC:=ftn}
+export CXX=${CXX:=CC}
+export BLAS_VENDOR=${BLAS_VENDOR:=OpenBLAS}
+export BML_OPENMP=yes
+export BML_ROCSPARSE=yes
+export PROGRESS_OPENMP=${PROGRESS_OPENMP:=yes}
+export INSTALL_DIR=${INSTALL_DIR:="${MY_PATH}/install"}
+export PROGRESS_GRAPHLIB=${PROGRESS_GRAPHLIB:=no}
+export PROGRESS_TESTING=${PROGRESS_TESTING:=yes}
+export CMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE:=RelWithDebInfo}
+export PROGRESS_EXAMPLES=${PROGRESS_EXAMPLES:=yes}
+export PROGRESS_BENCHMARKS=${PROGRESS_BENCHMARKS:=yes}
+export EXTRA_FCFLAGS=""
+export EXTRA_LINK_FLAGS=""
+./build.sh configure
+
---
+++ scripts/setenv_crusher_offload.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+module load craype-accel-amd-gfx90a
+module load rocm/5.1.0
+module load cmake
+module load openblas
