7292f668c1d2f30e801a42d65d23a2a25e546453
Jean-Luc Fattebert <fattebertj@ornl.gov>
2023-03-24 10:17:06 -0400

    Add BML_PREFIX_PATH build variable
    Clean up the build system a little bit

--- scripts/build_crusher_magma_gcc.sh
+++ scripts/build_crusher_magma_gcc.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
-
 rm -r build
 rm -r install
 
 # Set BML Library location
 MY_PATH=`pwd`
-export BML_DIR=${MY_PATH}/../bml/install
-# Configuring PROGRESS with OpenMP
-export MAGMA_PATH=${MAGMA_PATH:=${OLCF_MAGMA_ROOT}}
+export BML_DIR=/autofs/nccs-svm1_proj/csc304/bml/crusher/install
+
 export CC=${CC:=gcc}
 export FC=${FC:=gfortran}
 export CXX=${CXX:=g++}
@@ -18,8 +16,7 @@ export PROGRESS_TESTING=${PROGRESS_TESTING:=yes}
 export CMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE:=RelWithDebInfo}
 export PROGRESS_EXAMPLES=${PROGRESS_EXAMPLES:=yes}
 export PROGRESS_BENCHMARKS=${PROGRESS_BENCHMARKS:=yes}
-export PKG_CONFIG_PATH=${BML_LIB}/lib64/pkgconfig
-export EXTRA_FCFLAGS=""
-export EXTRA_LINK_FLAGS=""
+export BML_PREFIX_PATH=${BML_PREFIX_PATH:=$BML_DIR}
+
 ./build.sh configure
 
--- scripts/build_olcf_summit_gnu_mgma_openblas.sh
+++ scripts/build_olcf_summit_gnu_mgma_openblas.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 module load cmake
-module load cuda
-module load gcc/8.1.1
-module load netlib-lapack
-module load openblas
-module load magma
+module load gcc/9.1.0
 
 rm -r build
 rm -r install
 
 # Set BML Library location
-BML_LIB=/gpfs/alpine/csc304/proj-shared/bml
+BML_LIB=/autofs/nccs-svm1_proj/csc304/bml/summit_gcc_magma
 
 MY_PATH=`pwd`
 
+#get jsrun with full path
+JSRUN=$(which jsrun)
+echo ${JSRUN}
+
 # Configuring PROGRESS with OpenMP
-export MAGMA_PATH=${MAGMA_PATH:=${OLCF_MAGMA_ROOT}}
 export CC=${CC:=gcc}
 export FC=${FC:=gfortran}
 export CXX=${CXX:=g++}
@@ -25,6 +24,10 @@ export PROGRESS_GRAPHLIB=${PROGRESS_GRAPHLIB:=no}
 export PROGRESS_TESTING=${PROGRESS_TESTING:=yes}
 export CMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE:=RelWithDebInfo}
 export PROGRESS_EXAMPLES=${PROGRESS_EXAMPLES:=yes}
-export PKG_CONFIG_PATH=${BML_LIB}/lib64/pkgconfig
+export PROGRESS_BENCHMARKS=${PROGRESS_BENCHMARKS:=yes}
+export BML_PREFIX_PATH=${BML_PREFIX_PATH:=$BML_LIB}
+
+export PROGRESS_NONMPI_PRECOMMAND=${PROGRESS_NONMPI_PRECOMMAND:=${JSRUN}}
+export PROGRESS_NONMPI_PRECOMMAND_ARGS=${PROGRESS_NONMPI_PRECOMMAND_ARGS:="-n1;-a1;-g1;-c7;--smpiargs=off"}
 
 ./build.sh install
