b88c4fbee561769d2e05da39549fc016032c2e16
Jean-Luc Fattebert <fattebertj@ornl.gov>
2023-03-27 11:52:58 -0400

    Add option of repeating loops in dmconstruction
    This allows "warm up" of the GPU and timings statistics

--- benchmarks/dmconstruction/run.sh
+++ benchmarks/dmconstruction/run.sh
@@ -4,6 +4,7 @@
 
 export OMP_NUM_THREADS=12
 run=../../build/dmconstruction
+nreps=2
 
 device="myCPU" #Architecture name
 alg="sp2_alg1" #Name of the algorithm 
@@ -16,13 +17,13 @@ do
     fileout="times_${system}_${alg}_${device}_${format}.dat"
     rm $fileout
     #for i in 1000 2000 4000 6000 8000 10000 12000 14000 16000 18000 20000
-    for i in 100 200 400 
+    for i in 100 200 400
     do
       echo "Format, System, Size:" $format"," $system"," $i
       sed 's/NOrbs=.*/NOrbs= '$i'/g' input.in.$system > tmp
       sed 's/BMLType=.*/BMLType= '$format'/g' tmp > input.in
-      #jsrun -n1 -a1 -g1 -c21 -bpacked:21 ./main input.in > out$i$device$alg$format$system
-      $run  input.in > out$i$device$alg$format$system
+      #jsrun -n1 -a1 -g1 -c21 -bpacked:21 ./main input.in $nreps > out$i$device$alg$format$system
+      $run  input.in $nreps > out$i$device$alg$format$system
       time=`grep $tag out$i$device$alg$format$system | awk 'NF>1{print $NF}'`
       echo $i $time  >> $fileout
     done
