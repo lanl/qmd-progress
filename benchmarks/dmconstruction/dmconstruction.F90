71b095dd6abf657c5da0f512445dd211153a5f7b
Mike Wall <mewall@lanl.gov>
2023-05-12 11:29:19 -0400

    Minor Cray compiler bug fixes

--- benchmarks/dmconstruction/dmconstruction.F90
+++ benchmarks/dmconstruction/dmconstruction.F90
@@ -19,6 +19,7 @@ program hmodel
   integer, parameter ::  dp = kind(1.0d0)
   integer ::  norbs,seed,i,nreps
   integer ::  verbose
+  integer ::  iargct
   character(20) :: filename,arg
   type(bml_matrix_t) ::  ham_bml,rho_bml,rhos_bml,evects_bml,aux_bml
   type(mham_type) ::  mham
@@ -27,6 +28,8 @@ program hmodel
   real(dp), allocatable :: eigenvalues(:)
   real(dp) :: ef,sparsity,dec,mlsi,mlsf,bnorm
   character(20) :: bml_dmode
+
+  integer, external :: iargc
 
   call prg_initParallel()
 
