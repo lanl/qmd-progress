1adab9124887d82042c48b32225e36d4b5240772
ares201005 <zhy@lanl.gov>
2023-07-06 17:14:57 -0400

    Finalized c interface for density matrix module C test for density matrix module Python script to generate c wrappers and headers Add c interface for prg_chebyshev_mod module (Tests TBA) Add c interface for prg_charges_mod (Tests TBA)

---
+++ examples/c_interface/scf.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include "bml.h"
+
+void main(){
+    int dp = sizeof(double);   // equivalent of kind(1.0d0)
+    int i, nel, norb, j;
+    int** hindex = NULL;        // allocatable in Fortran, use malloc or calloc in C when actual size is known
+    char (*intKind)[4] = NULL; // 3 char + null termination '\0'
+
+    double bndfil, scferror;
+    double* charges_old = NULL;
+    double** coul_forces_k = NULL;
+    double** coul_forces_r = NULL;
+    double* coul_pot_k = NULL;
+    double* coul_pot_r = NULL;
+    double** dqin = NULL;
+    double** dqout = NULL;
+
+    bml_matrix_t ham0_bml, ham_bml, orthoh_bml, orthop_bml;
+    bml_matrix_t over_bml, rho_bml, zmat_bml;
+    
+    latte_type lt;
+    sp2data_type sp2;
+    system_type sy;
+    tbparams_type tb;
+
+    intpairs_type** intPairsH = NULL;
+    intpairs_type** intPairsS = NULL;
+
+    double** onsitesH = NULL;
+    double** onsitesS = NULL;
+
+    char (*TypeA)[3] = NULL; // 2 char + null termination '\0'
+    char (*TypeB)[3] = NULL; // 2 char + null termination '\0'
+
+
+} 
