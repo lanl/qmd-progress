1ca6a206fe83f2f8776d17ee289dbf4ff3f34090
ares201005 <zhy@lanl.gov>
2023-07-19 11:28:32 -0400

    C interface for density matrix, including sp2 C test for density matrix module and sp2 methods C interface for prg_chebyshev_mod module C interface for prg_charges_mod Python script to generate c wrappers and headers

---
+++ scripts/transform.py
@@ -0,0 +1,219 @@
+import re
+
+import copy
+
+r"""
+This is the python script to generate c wrappers and corresponding
+headers for fortran (public) subroutines.
+
+It scans the files in src folder and generate the c wrappers in prg_c_interface.F90 and
+headers in prg_progress_mod.h.
+
+Note: the generated wrappers are not bug-free, it is only used to facilitate building
+c interrface.
+"""
+
+def get_public(fortran_code):
+
+   # Define the regex pattern to match lines starting with 'public ::'
+   pattern = re.compile(r'public *::(.*)')
+
+   # Find matches in the fortran code
+   matches = pattern.findall("".join(fortran_code))
+
+   public_subroutines = []
+
+   for match in matches:
+      # Split by comma to get individual subroutines, strip whitespace
+      subroutines = [subroutine.strip() for subroutine in match.split(',')]
+      public_subroutines.extend(subroutines)
+
+   # print the list of public subroutines
+   print("Public subroutines are \n")
+   for subroutine in public_subroutines:
+       print(f"  public :: {subroutine}_c")
+   print("")
+   return public_subroutines
+
+
+def transform_fortran_file(filename):
+    # use to find the argument names of each subroutine
+    argument_re = re.compile(r'(real\(8\)|real\(dp\)|real\(PREC\)|integer|logical|type\(bml_matrix_t\)|character\(\d+\)|character\(len=\*\))(,.+?)?\s+::\s+([a-zA-Z0-9_,\s\(\):]+)')
+
+    # Replacement rules for argument types
+    # i.e., fortran to c datatype mapping
+    type_replacement = {
+        "real(8)": "real(c_double)",
+        "real(dp)": "real(c_double)",
+        "real(PREC)": "real(c_double)",
+        "type(bml_matrix_t)": "type(c_ptr)",
+        "integer": "integer(c_int)",
+        "character(len=*)":"character(c_char)",
+        "character(10)":"character(c_char)",
+        "character(20)":"character(c_char)",
+        "character(50)":"character(c_char)",
+        "character(3)":"character(c_char)",
+        "character(2)":"character(c_char)",
+        "logical":"logical(c_bool)"
+    }
+
+    # Replacement rules for argument types (for c header)
+    type_replacement_header = {
+        "real(8)": "double",
+        "real(dp)": "double",
+        "real(PREC)": "double",
+        "type(bml_matrix_t)": "bml_matrix_t*",
+        "integer": "int",
+        "character(len=*)":"char*",
+        "character(10)":"char*",
+        "character(20)":"char*",
+        "character(50)":"char*",
+        "character(3)":"char*",
+        "character(2)":"char*",
+        "logical":"int*"
+    }
+
+    with open(filename, "r") as f:
+        content = f.readlines()
+
+    public_subroutines = get_public(content)
+
+    new_content = []
+    header_content = []
+    is_inside_subroutine = False
+    argument_names = []
+    argument_declarations = []
+    argument_assignments = []
+    subroutine_name = ""
+    bml_matrix_t_args = []
+
+    for k, line in enumerate(content):
+        if "&" in line:
+            line = line.replace("\n","").strip()
+            line = line + content[k+1].strip()
+            line = line.replace("&", "")
+        match = subroutine_re.match(line.strip())
+        if match:
+            subroutine_name = match.group(1)
+            argument_names = [arg.strip() for arg in match.group(2).split(',')]
+            for iarg, argname in enumerate(argument_names):
+                if "&" in argname:
+                    print(f"debug-zy: {argname} has character &")
+                    argument_names[iarg] = argname.split("&")[0]
+
+
+            ispublic = subroutine_name in public_subroutines
+            print(f"\ndebug-zy: a subroutine {subroutine_name} is found!", ispublic)
+            print(f"debug-zy: its arguments are: {argument_names}")
+            # skip the subroutine if it's not public
+            if not ispublic: continue
+            is_inside_subroutine = True
+            arg2typ = {}
+            continue
+
+        if is_inside_subroutine:
+            if 'end subroutine' in line:
+                argument_names_c = [arg+"_c" if arg in bml_matrix_t_args else arg for arg in argument_names]
+                new_subroutine_header = f'  subroutine {subroutine_name}_c({", ".join(argument_names_c)}) bind(C, name="{subroutine_name}")'
+                new_subroutine_body = '\n'.join(argument_declarations + argument_assignments)
+                new_subroutine_call = f'    call {subroutine_name}({", ".join(argument_names)})'
+                new_content.append(new_subroutine_header)
+                new_content.append(new_subroutine_body)
+                new_content.append(new_subroutine_call)
+
+                new_content.append('  end subroutine ' + subroutine_name + '_c\n')
+                tmpheader = []
+                for arg in argument_names:
+                    if arg in arg2typ:
+                        tmpheader.append(f'{type_replacement_header[arg2typ[arg]]} {arg}')
+                header_content.append(f'\nvoid {subroutine_name}({", ".join(tmpheader)});')
+
+                is_inside_subroutine = False
+                argument_names = []
+                argument_declarations = []
+                argument_assignments = []
+                bml_matrix_t_args = []
+                continue
+
+            match = argument_re.match(line.strip())
+            if match:
+                arg_type = match.group(1)
+                arg_modifiers = match.group(2)
+                print("matched group(3)=", match.group(3))
+
+                # Remove any spaces within array declarations
+                input_string = match.group(3)
+                # Split the input string by comma using lookahead assertion
+                arg_names = re.split(r',(?![^(]*\))', input_string)
+                arg_names = [arg.strip() for arg in arg_names]
+                print("debug: arg_names=", arg_names)
+
+                # create arg to type mapping for c header
+                for arg_name in arg_names:
+                    if "(" in arg_name:
+                        arg_name = arg_name.split("(")[0]
+                    print(f"debug-zy: arg_name={arg_name}, type is {arg_type}")
+                    if arg_name in argument_names:
+                        arg2typ[arg_name] = arg_type
+
+                if arg_type in type_replacement:
+                    new_arg_type = type_replacement[arg_type]
+                    print("new arg_type is:", new_arg_type)
+                    if arg_modifiers is None:
+                        arg_modifiers = ", value"
+                    else:
+                        arg_modifiers = arg_modifiers.replace("intent(inout)", "value")
+                        arg_modifiers = arg_modifiers.replace("intent(in)", "value")
+                        if "allocatable" in arg_modifiers or "optional" in arg_modifiers:
+                            arg_modifiers = arg_modifiers.replace("value", "")
+
+                    for arg_name in arg_names:
+                        tmparg_modifiers = copy.copy(arg_modifiers)
+
+                        if "(:,:)" in arg_name:
+                            tmparg_name = arg_name[:-5]
+                            tmparg_modifiers = tmparg_modifiers.replace("value", "target")
+                        elif "(:)" in arg_name:
+                            tmparg_name = arg_name[:-3]
+                            tmparg_modifiers = tmparg_modifiers.replace("value", "target")
+                        elif "(" in arg_name:
+                            tmparg_name = arg_name.split("(")[0]
+                            tmparg_modifiers = tmparg_modifiers.replace("value", "target")
+                        else:
+                            tmparg_name = arg_name
+                        print("debug: arg_name/tmparg_name=", arg_name, tmparg_name)
+                        print("debug: argument_names=", argument_names)
+
+                        if tmparg_name in argument_names:
+                        #if arg_name in argument_names:
+                            print(f"{tmparg_name} is added to the declaration\n")
+                            if arg_type == "type(bml_matrix_t)":
+                                bml_matrix_t_args.append(arg_name)
+                                argument_declarations.append(f'    {new_arg_type}{tmparg_modifiers} :: {arg_name}_c')
+                                argument_declarations.append(f'    {arg_type} :: {arg_name}')
+                                argument_assignments.append(f'    {arg_name}%ptr = {arg_name}_c')
+                            else:
+                                argument_declarations.append(f'    {new_arg_type}{tmparg_modifiers} :: {arg_name}')
+                        else:
+                            print(f"{tmparg_name} is not added to the declaration\n")
+
+    new_filename = filename[:-4] + "_c.F90"
+    print("c_wrappers are:")
+    with open(new_filename, "w") as f:
+        print('\n'.join(new_content))
+        f.write('\n'.join(new_content))
+
+    print("c headers are:")
+    #header_filename = filename[:-4] + ".h"
+    header_filename = "prg_progress_mod.h"
+    with open(header_filename, "a") as f:
+        print('\n'.join(header_content))
+        f.write('\n'.join(header_content))
+    f.close()
+
+# Example usage
+import glob, sys
+
+files=glob.glob(f"src/*mod.F90")
+for fname in files:
+    transform_fortran_file(fname)
---
+++ src/prg_progress_mod.h
@@ -0,0 +1,529 @@
+
+#ifndef PROGRESS_INTERFACE_H
+#define PROGRESS_INTERFACE_H
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#include <stdbool.h>
+#include <bml.h>
+
+    extern void prg_version(
+        );
+    extern void prg_progress_init(
+        );
+    extern void prg_progress_shutdown(
+        );
+
+//-------- prg_densitymatrix_mod headers ------------------------------------
+    void prg_build_density_T0(
+    int norbs,
+    bml_matrix_t * ham_bml,
+    bml_matrix_t * rho_bml,
+    double threshold,
+    double bndfil,
+    double *eigenvalues_out);
+
+    void prg_build_density_T(
+    int norbs,
+    bml_matrix_t * ham_bml,
+    bml_matrix_t * rho_bml,
+    double threshold,
+    double bndfil,
+    double kbt,
+    double ef,
+    double *eigenvalues_out);
+
+    void prg_build_density_T_fulldata(
+    int norbs,
+    bml_matrix_t * ham_bml,
+    bml_matrix_t * rho_bml,
+    double threshold,
+    double bndfil,
+    double kbt,
+    double ef,
+    double *eigenvalues_out,
+    bml_matrix_t * evects_bml,
+    double *fvals_out);
+
+    void prg_build_density_T_ed(
+    int norbs,
+    bml_matrix_t * ham_bml,
+    bml_matrix_t * rho_bml,
+    bml_matrix_t * evects_bml,
+    double threshold,
+    double bndfil,
+    double kbt,
+    double ef,
+    double *evals,
+    double *dvals,
+    int **hindex,
+    int llsize,
+    int verbose);
+
+    void prg_get_evalsDvalsEvects(
+    int norbs,
+    bml_matrix_t * ham_bml,
+    double threshold,
+    int **hindex,
+    int llsize,
+    double *evals,
+    double *dvals,
+    bml_matrix_t * evects_bml,
+    int verbose);
+
+    void prg_build_density_fromEvalsAndEvects(
+    int norbs,
+    bml_matrix_t * evects_bml,
+    bml_matrix_t * rho_bml,
+    double threshold,
+    double bndfil,
+    double kbt,
+    double ef,
+    int verbose);
+
+    void prg_build_density_T_fermi(
+    bml_matrix_t * ham_bml,
+    bml_matrix_t * rho_bml,
+    double threshold,
+    double kbt,
+    double ef,
+    int verbose,
+    double drho);
+
+    void prg_build_atomic_density(
+    bml_matrix_t * rhoat_bml,
+    int norb,
+    char *bml_type);
+
+    void prg_get_flevel(
+    double kbt,
+    double bndfil,
+    double tol,
+    double Ef,
+    int *err);
+
+    void prg_get_flevel_nt(
+    double kbt,
+    double bndfil,
+    double tol,
+    double ef,
+    int *err,
+    int verbose);
+
+    void prg_get_eigenvalues(
+    int norbs,
+    bml_matrix_t * ham_bml,
+    double *eigenvalues,
+    int verbose);
+
+    void prg_check_idempotency(
+    bml_matrix_t * mat_bml,
+    double threshold,
+    double idempotency);
+
+    void prg_toEigenspace(
+    bml_matrix_t * mat_bml,
+    bml_matrix_t * matEig_bml,
+    bml_matrix_t * evects_bml,
+    double threshold,
+    int verbose);
+
+    void prg_toCanonicalspace(
+    bml_matrix_t * mat_bml,
+    bml_matrix_t * matCan_bml,
+    bml_matrix_t * evects_bml,
+    double threshold,
+    int verbose);
+
+    void Canon_DM_PRT(
+    double T,
+    double mu0,
+    int m,
+    int HDIM);
+
+//-----prg_charges_mod headers -----------------------------------------
+    void prg_get_charges(
+    int nats,
+    int norbs,
+    bml_matrix_t * rho_bml,
+    bml_matrix_t * over_bml,
+    int **hindex,
+    double *charges,
+    double *numel,
+    int *spindex,
+    int mdimin,
+    double threshold);
+
+    void prg_get_hscf(
+    int nats,
+    bml_matrix_t * ham0_bml,
+    bml_matrix_t * over_bml,
+    bml_matrix_t * ham_bml,
+    int *spindex,
+    int **hindex,
+    double *hubbardu,
+    double *charges,
+    double *coulomb_pot_r,
+    double *coulomb_pot_k,
+    int mdimin,
+    double threshold);
+
+    void prg_get_hscf_v2(
+    int nats,
+    bml_matrix_t * ham0_bml,
+    bml_matrix_t * over_bml,
+    bml_matrix_t * ham_bml,
+    int *spindex,
+    int **hindex,
+    double *hubbardu,
+    double *charges,
+    double *coulomb_pot_r,
+    double *coulomb_pot_k,
+    int mdimin,
+    double threshold);
+
+//------prg_chebyshev_mod headers --------------------------------------
+//void prg_parse_cheb(char* filename);
+
+    void prg_build_density_cheb(
+    bml_matrix_t * ham_bml,
+    bml_matrix_t * rho_bml,
+    double athr,
+    double threshold,
+    int ncoeffs,
+    double kbt,
+    double ef,
+    double bndfil,
+    int jon,
+    int verbose);
+
+    void prg_build_density_cheb_fermi(
+    bml_matrix_t * ham_bml,
+    bml_matrix_t * rho_bml,
+    double athr,
+    double threshold,
+    int ncoeffs,
+    double kbt,
+    double ef,
+    double bndfil,
+    int getef,
+    double fermitol,
+    int jon,
+    int npts,
+    int trkfunc,
+    int verbose);
+
+//-----prg_dos_mod -----------------------------------------------------
+
+    void prg_write_tdos(
+    int nstates,
+    double *eigenvals,
+    double gamma,
+    int npts,
+    double emin,
+    double emax,
+    char *filename);
+
+// prg_ewald_mod
+    void Ewald_Real_Space_Single_latte(
+    double COULOMBV,
+    int I,
+    double RXYZ,
+    double Box,
+    int Nr_elem,
+    double DELTAQ,
+    int J,
+    double U,
+    int Element_Pointer,
+    int Nr_atoms,
+    double COULACC,
+    int HDIM,
+    int Max_Nr_Neigh);
+
+    void Ewald_Real_Space_Single(
+    double COULOMBV,
+    double FCOUL,
+    int I,
+    double RX,
+    double RY,
+    double RZ,
+    double LBox,
+    double DELTAQ,
+    int J,
+    double U,
+    char *Element_Type,
+    int Nr_atoms,
+    double COULACC,
+    double TIMERATIO,
+    int HDIM,
+    int Max_Nr_Neigh);
+
+    void Ewald_Real_Space_Matrix_latte(
+    double E,
+    double RXYZ,
+    double Box,
+    double U,
+    int Element_Pointer,
+    int Nr_atoms,
+    double COULACC,
+    int nebcoul,
+    int totnebcoul,
+    int HDIM,
+    int Max_Nr_Neigh,
+    int Nr_Elem);
+
+    void Ewald_Real_Space_latte(
+    double COULOMBV,
+    int I,
+    double RXYZ,
+    double Box,
+    double DELTAQ,
+    double U,
+    int Element_Pointer,
+    int Nr_atoms,
+    double COULACC,
+    int nebcoul,
+    int totnebcoul,
+    int HDIM,
+    int Max_Nr_Neigh,
+    int Nr_Elem);
+
+    void Ewald_Real_Space_Test(
+    double COULOMBV,
+    int I,
+    double RX,
+    double RY,
+    double RZ,
+    double LBox,
+    double DELTAQ,
+    double U,
+    char *Element_Type,
+    int Nr_atoms,
+    double COULACC,
+    double nnRx,
+    double nnRy,
+    double nnRz,
+    int nrnnlist,
+    int nnType,
+    int Max_Nr_Neigh);
+
+    void Ewald_Real_Space(
+    double COULOMBV,
+    double FCOUL,
+    int I,
+    double RX,
+    double RY,
+    double RZ,
+    double LBox,
+    double DELTAQ,
+    double U,
+    char *Element_Type,
+    int Nr_atoms,
+    double COULACC,
+    double TIMERATIO,
+    double nnRx,
+    double nnRy,
+    double nnRz,
+    int nrnnlist,
+    int nnType,
+    int HDIM,
+    int Max_Nr_Neigh);
+    void prg_print_matrix(
+    char *matname,
+    double amat,
+    int i1,
+    int i2,
+    int j1,
+    int j2);
+
+// prg_sp2_fermi headers
+    void prg_sp2_fermi_init_norecs(
+    bml_matrix_t * h_bml,
+    int nsteps,
+    double nocc,
+    double tscale,
+    double threshold,
+    double occErrLimit,
+    double traceLimit,
+    bml_matrix_t * x_bml,
+    double mu,
+    double beta,
+    double h1,
+    double hN,
+    int sgnlist,
+    int verbose);
+
+    void prg_sp2_fermi(
+    bml_matrix_t * h_bml,
+    int osteps,
+    int nsteps,
+    double nocc,
+    double mu,
+    double beta,
+    double h1,
+    double hN,
+    int sgnlist,
+    double threshold,
+    double eps,
+    double traceLimit,
+    bml_matrix_t * x_bml);
+
+    void prg_sp2_entropy_function(
+    double mu,
+    double h1,
+    double hN,
+    int nsteps,
+    int sgnlist,
+    double GG,
+    double ee);
+
+
+// prg_sp2_mode headers
+    void prg_sp2_basic(
+    bml_matrix_t * h_bml,
+    bml_matrix_t * rho_bml,
+    double threshold,
+    double bndfil,
+    int minsp2iter,
+    int maxsp2iter,
+    char *sp2conv,
+    double idemtol,
+    int verbose);
+
+    void prg_sp2_basic_tcore(
+    bml_matrix_t * h_bml,
+    bml_matrix_t * rho_bml,
+    bml_matrix_t * rhofull_bml,
+    double threshold,
+    double bndfil,
+    int minsp2iter,
+    int maxsp2iter,
+    char *sp2conv,
+    double idemtol,
+    int verbose);
+
+    void prg_sp2_alg2(
+    bml_matrix_t * h_bml,
+    bml_matrix_t * rho_bml,
+    double threshold,
+    double bndfil,
+    int minsp2iter,
+    int maxsp2iter,
+    char *sp2conv,
+    double idemtol,
+    int verbose);
+
+    void prg_sp2_alg2_genseq(
+    bml_matrix_t * h_bml,
+    bml_matrix_t * rho_bml,
+    double threshold,
+    double bndfil,
+    int minsp2iter,
+    int maxsp2iter,
+    char *sp2conv,
+    double idemtol,
+    int pp,
+    int icount,
+    double vv,
+    int verbose);
+
+    void prg_sp2_alg2_seq(
+    bml_matrix_t * h_bml,
+    bml_matrix_t * rho_bml,
+    double threshold,
+    int pp,
+    int icount,
+    double vv,
+    int verbose);
+
+    void prg_prg_sp2_alg2_seq_inplace(
+    bml_matrix_t * h_bml,
+    bml_matrix_t * rho_bml,
+    double threshold,
+    int pp,
+    int icount,
+    double vv,
+    double mineval,
+    double maxeval,
+    int verbose);
+
+    void prg_sp2_alg1(
+    bml_matrix_t * h_bml,
+    bml_matrix_t * rho_bml,
+    double threshold,
+    double bndfil,
+    int minsp2iter,
+    int maxsp2iter,
+    char *sp2conv,
+    double idemtol,
+    int verbose);
+
+    void prg_sp2_alg1_seq(
+    bml_matrix_t * h_bml,
+    bml_matrix_t * rho_bml,
+    double threshold,
+    int pp,
+    int icount,
+    double vv);
+
+    void prg_prg_sp2_alg1_seq_inplace(
+    double threshold,
+    int pp,
+    int icount,
+    double vv,
+    double mineval,
+    double maxeval);
+
+    void prg_sp2_submatrix(
+    double threshold,
+    int pp,
+    int icount,
+    double vv,
+    double mineval,
+    double maxeval,
+    int core_size);
+
+    void prg_sp2_submatrix_inplace(
+    double threshold,
+    int pp,
+    int icount,
+    double vv,
+    double mineval,
+    double maxeval,
+    int core_size);
+
+
+
+// prg_timer_mod headers
+    void timer_prg_init(
+        );
+
+    void prg_timer_shutdown(
+        );
+
+    void prg_timer_start(
+    int itimer,
+    char *tag);
+
+    void prg_timer_stop(
+    int itimer,
+    int verbose);
+
+    void prg_timer_collect(
+        );
+
+    void prg_timer_results(
+        );
+
+    void prg_print_date_and_time(
+    char *tag);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif                          /* PROGRESS_INTERFACE_H */
