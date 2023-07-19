d7e9353b604b90fcf3f045e32727c577f5e9d2ee
cnegre <christianfannegre@gmail.com>
2023-07-19 10:38:13 -0600

    Added build

--- docs/build.sh
+++ docs/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-export PROJECT_NAME=${CMAKE_BUILD_TYPE:="PROGRESS"}
-export PROJECT_VERSION=${PROJECT_VERSION:="master"}
-export PROJECT_DESCRIPTION=${PROJECT_DESCRIPTION:="This library is focused on the development of general solvers that are commonly used in quantum chemistry packages."}
+export PROJECT_NAME=${CMAKE_BUILD_TYPE:="PROGRESS"} 
+export PROJECT_VERSION=${PROJECT_VERSION:="master"} 
+export PROJECT_DESCRIPTION=${PROJECT_DESCRIPTION:=""}
 sed -i -e 's/@PROJECT_NAME@/'$PROJECT_NAME'/g' Doxyfile.in
 sed -i -e 's/@PROJECT_VERSION@/'$PROJECT_VERSION'/g' Doxyfile.in
 sed -i -e 's/@PROJECT_DESCRIPTION@/'$PROJECT_DESCRIPTION'/g' Doxyfile.in
