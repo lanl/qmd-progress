4ca4a60668c00147f8a9d352226af650f8eda754
cnegre <christianfannegre@gmail.com>
2023-12-13 11:41:55 -0700

    Added bio only Dm solve

--- benchmarks/dmconstruction_graphBased/build_cmake.sh
+++ benchmarks/dmconstruction_graphBased/build_cmake.sh
@@ -5,6 +5,7 @@ METIS_LIB="$HOME/metis-5.1.0/lib"
 METIS_INCLUDE="$HOME/metis-5.1.0/include"
 PROGRESS="$HOME/qmd-progress/install/"
 BML="$HOME/bml/install"
-
-cmake  -DCMAKE_PREFIX_PATH="$BML;$PROGRESS;$METIS_LIB;$METIS_INCLUDE" ../
+BML_INCLUDE="$HOME/bml/install/include"
+FC="/usr/bin/mpif90"
+cmake  -DCMAKE_PREFIX_PATH="$BML;$BML_INCLUDE;$PROGRESS;$METIS_LIB;$METIS_INCLUDE" ../
 make  
