22ee49048563bd9ff2775d7329d7e6ccede9af47
Mike Wall <mewall@lanl.gov>
2024-07-05 11:54:01 -0600

    Add TrpCage example for gpmdk

---
+++ examples/gpmdk/run/TrpCage/wrapper.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+export CUDA_VISIBLE_DEVICES=$(echo "${SLURM_LOCALID}%4" |bc)
+
+#RUN="nsys profile /usr/projects/icapt/mewall/packages/gpmd/gpmd/build_gpu/gpmdcov"
+#RUN="map --profile /usr/projects/icapt/mewall/packages/gpmd/gpmd/build_forge/gpmdcov"
+RUN="/usr/projects/icapt/mewall/venado/packages/qmd-progress/examples/gpmdk/build_hackathon/gpmdk"
+#stdbuf -o0 $RUN input_ICH.in |& tee out_ICH
+if [ $SLURM_PROCID -eq 0 ]; then
+	stdbuf -o0 $RUN input.in >& out_0
+else
+	$RUN input.in >& /dev/null
+fi
