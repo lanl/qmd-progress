17ad747945dcf18e9d34241cb4359b575629badf
Mike Wall <mewall@lanl.gov>
2024-07-05 11:54:01 -0600

    Add TrpCage example for gpmdk

---
+++ examples/gpmdk/run/TrpCage/sbatch.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+##SBATCH -p gpu
+##SBATCH -p gpu_debug
+##SBATCH --reservation=gpu_debug
+##SBATCH --qos=debug 
+#SBATCH --time 2:00:00
+#SBATCH -N 64
+##SBATCH -A ichelp_g
+##SBATCH -A w23_macroqmd_g
+
+cd ${PWD}
+source /usr/projects/icapt/mewall/venado/packages/qmd-progress/scripts/setenv_venado.sh
+
+#export MPICH_ALLREDUCE_NO_SMP=1
+export MPICH_SMP_SINGLE_COPY_MODE=NONE
+#export MPICH_OPT_THREAD_SYNC=0
+
+OMP_NUM_THREADS=72 srun -n 256 --ntasks-per-node=4 --cpus-per-task=72 bash wrapper.sh   
+
---
+++ examples/gpmdk/run/TrpCage/sbatch_profile.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+##SBATCH -p gpu
+##SBATCH -p gpu_debug
+##SBATCH --reservation=gpu_debug
+##SBATCH --qos=debug 
+##SBATCH --time 2:00:00
+#SBATCH -N 32
+##SBATCH -A ichelp_g
+##SBATCH -A w23_macroqmd_g
+
+cd ${PWD}
+source /usr/projects/icapt/mewall/venado/packages/qmd-progress/scripts/setenv_venado.sh
+
+#export MPICH_ALLREDUCE_NO_SMP=1
+export MPICH_SMP_SINGLE_COPY_MODE=NONE
+
+OMP_NUM_THREADS=72 srun -n 128 --ntasks-per-node=4 --cpus-per-task=72 bash wrapper_profile.sh   
+
