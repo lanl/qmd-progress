ff1de5eaac2ac66ac8b5844ea857df1c61900d64
Mike Wall <mewall@lanl.gov>
2024-07-19 16:45:19 -0600

    Update build script

--- examples/gpmdk/build_venado_hackathon_nvtx.sh
+++ examples/gpmdk/build_venado_hackathon_nvtx.sh
@@ -3,11 +3,8 @@ rm -rf build_hackathon
 mkdir build_hackathon
 cd build_hackathon
 cmake  -DCMAKE_Fortran_COMPILER="ftn" -DPROGRESS_MPI="yes" -DLIB="no" -DGPMDK_NVTX="yes" \
--DEXTRA_FCFLAGS="-g -O2 -Wall -Wunused -ffree-line-length-none -ffpe-trap=invalid,overflow,zero -lnvToolsExt" \
+-DEXTRA_FCFLAGS="-g -O2 -Wall -Wunused -fopenmp -ffree-line-length-none -ffpe-trap=invalid,overflow,zero -lnvToolsExt" \
 -DCMAKE_PREFIX_PATH="$WORKDIR/qmd-progress/install_hackathon/;$WORKDIR/qmd-progress/bml/install_hackathon;$WORKDIR/metis-5.1.0/install"  ../src/
 #-DEXTRA_FCFLAGS="-g -O2 -DHPCTOOLKIT_PROFILE -Wall -Wunused -fopenmp -ffree-line-length-none -ffpe-trap=invalid,overflow,zero -L$HPCTOOLKIT/lib/hpctoolkit -lhpctoolkit" \
 make -j1
 
-#cmake  -DCMAKE_Fortran_COMPILER="ftn" -DPROGRESS_MPI="yes" -DLIB="no" -DGPMDK_NVTX="yes" \
-#-DEXTRA_FCFLAGS="-g -O2 -Wall -Wunused -fopenmp -ffree-line-length-none -ffpe-trap=invalid,overflow,zero -lnvToolsExt" \
-#-DCMAKE_PREFIX_PATH="$WORKDIR/qmd-progress/install_hackathon/;$WORKDIR/qmd-progress/bml/install_hackathon;$WORKDIR/metis-5.1.0/install"  ../src/
