372c8ca53b6b0c56263c292c658258a535daffc5
Mike Wall <mewall@lanl.gov>
2024-08-07 11:31:00 -0600

    Beginning update of graph MPI

--- examples/gpmdk/run/water/run.sh
+++ examples/gpmdk/run/water/run.sh
@@ -7,7 +7,8 @@
 #OMP_NUM_THREADS=36 mpiexec -np 1 $RUN input.in | tee out
 
 #OMP_NUM_THREADS=36 mpirun -np 3 --map-by socket:pe=18 --bind-to core:overload-allowed  $RUN input.in | tee out
-OMP_NUM_THREADS=32 srun -n 4 -c 32 bash wrapper.sh   
+#OMP_NUM_THREADS=32 srun -n 4 -c 32 bash wrapper.sh   
+OMP_NUM_THREADS=64 srun -n 1 -c 64 bash wrapper.sh   
 #OMP_NUM_THREADS=64 $RUN input.in | tee out  
 #OMP_NUM_THREADS=36 $RUN input.in | tee out 
 #$RUN input.in | tee out 
