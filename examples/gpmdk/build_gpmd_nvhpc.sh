ac0c9be9e0bdc4c82bcc7d2519397de98f6eccc3
Mike Wall <mewall@lanl.gov>
2024-09-09 09:08:02 -0600

    Report max time and rank for dH+dS

--- examples/gpmdk/build_gpmd_nvhpc.sh
+++ examples/gpmdk/build_gpmd_nvhpc.sh
@@ -3,7 +3,7 @@ rm -rf build_nvhpc
 mkdir build_nvhpc
 cd build_nvhpc
 cmake  -DCMAKE_Fortran_COMPILER="mpifort" -DPROGRESS_MPI="yes" -DGPMDK_NVTX="yes" \
--DEXTRA_FCFLAGS="-g -mp -L${NVHPC_ROOT}/cuda/lib64 -lnvToolsExt" \
+-DEXTRA_FCFLAGS="-g -mp -O2 -L${NVHPC_ROOT}/cuda/lib64 -lnvToolsExt" \
 -DCMAKE_PREFIX_PATH="$WORKDIR/qmd-progress/install_nvhpc/;$WORKDIR/qmd-progress/bml/install_nvhpc;$WORKDIR/metis-5.1.0/install"  ../src/
 make
 
