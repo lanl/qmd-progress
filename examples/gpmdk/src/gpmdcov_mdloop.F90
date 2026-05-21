995998910c17c1278105ce297658bd161d57eb49
Mike Wall <mewall@lanl.gov>
2026-05-21 14:10:05 -0600

    Use sedacs neighborlist

--- examples/gpmdk/src/gpmdcov_mdloop.F90
+++ examples/gpmdk/src/gpmdcov_mdloop.F90
@@ -419,14 +419,8 @@ contains
 #ifdef USE_NVTX
            call gpmdStartRange("build_nlist_sparse_sedacs",3)
 #endif
-           !call gpmdcov_destroy_nlist(nl2,lt%verbose)
-#ifdef USE_OFFLOAD
            call gpmdcov_build_nlist_sedacs(sy%coordinate,sy%lattice_vector,coulcut,nl,lt%verbose,myRank,numRanks)
-#else
-           !call gpmdcov_build_nlist_sedacs(sy%coordinate,sy%lattice_vector,coulcut,nl,lt%verbose,myRank,numRanks)
-           !call gpmdcov_build_nlist_sparse_v2(sy%coordinate,sy%lattice_vector,coulcut,nl,lt%verbose,myRank,numRanks)
-           call gpmdcov_build_nlist_sparse_v2(sy%coordinate,sy%lattice_vector,coulcut,nl,lt%verbose,myRank,numRanks)
-#endif
+
            ! if(any(nl2%nrnnstruct.ne.nl%nrnnstruct))then
            !    write(*,*)"DEBUG: nrnnstruct not equal"
            !    do k = 1,size(nl%nrnnstruct)
